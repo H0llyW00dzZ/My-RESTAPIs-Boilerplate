38bd75c800d61f2177b07214982d4f814147d7be
H0llyW00dzZ <priv8@btz.pm>
2024-04-13 00:01:13 +0700

    Implement Middleware Cache & Rate Limiter (#7)
    * Implement Middleware Cache
    
    - [+] feat(middleware/utils.go): add NewCacheMiddleware function for cache configuration
    - [+] chore(go.mod, go.sum): update dependencies
    
    * Implement Middleware Rate Limiter
    
    - [+] feat(utils.go): add NewRateLimiter function to create rate limiter middleware
    - [+] fix(utils.go): add logger import to log user activity when rate limit is reached

---
+++ backend/internal/middleware/utils.go
@@ -0,0 +1,49 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package middleware
+
+import (
+	"h0llyw00dz-template/backend/internal/database"
+	"time"
+
+	log "h0llyw00dz-template/backend/internal/logger"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+)
+
+// NewCacheMiddleware creates a new cache middleware with the specified expiration time and cache control flag.
+// It retrieves the Redis storage interface from the provided database service and configures the cache middleware accordingly.
+func NewCacheMiddleware(db database.Service, expiration time.Duration, cacheControl bool) fiber.Handler {
+	// Retrieve the Redis storage interface from the database service.
+	cacheMiddlewareService := db.FiberStorage()
+	// Create a new cache middleware with the desired configuration.
+	return cache.New(cache.Config{
+		Expiration:   expiration,
+		CacheControl: cacheControl,
+		Storage:      cacheMiddlewareService,
+	})
+}
+
+// NewRateLimiter creates a new rate limiter middleware with the specified maximum number of requests,
+// expiration time, and a custom message to log when the rate limit is reached.
+// It retrieves the Redis storage interface from the provided database service and configures the rate limiter middleware accordingly.
+func NewRateLimiter(db database.Service, max int, expiration time.Duration, limitReachedMessage string) fiber.Handler {
+	// Retrieve the Redis storage interface from the database service.
+	rateLimiterService := db.FiberStorage()
+	// Create a new rate limiter middleware with the desired configuration.
+	return limiter.New(limiter.Config{
+		Storage:    rateLimiterService,
+		Max:        max,
+		Expiration: expiration,
+		LimitReached: func(c *fiber.Ctx) error {
+			log.LogUserActivity(c, limitReachedMessage)
+			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
+				"error": fiber.ErrTooManyRequests.Message,
+			})
+		},
+	})
+}
