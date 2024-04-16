081e9ec7da80ad57408167b52f4d052c73a86d9c
H0llyW00dzZ <priv8@btz.pm>
2024-04-16 10:19:24 +0700

    Implement [Internal] [Middleware] REST APIs Routes
    - [+] feat(middleware): add rate limiting and REST API routes registration
    - [+] feat(middleware): integrate REST API routes registration into RegisterRoutes function

--- backend/internal/middleware/constant.go
+++ backend/internal/middleware/constant.go
@@ -4,9 +4,25 @@
 
 package middleware
 
+import "time"
+
 const (
 	// MsgPanicOccurred is the log message indicating that a panic occurred within the application.
 	MsgPanicOccurred = "Panic occurred: %v"
 	// MsgStackTrace is the log message containing the stack trace of a panic.
 	MsgStackTrace = "Stack trace:\n%s"
+	// MsgRESTAPIsVisitorGotRateLimited is the log message indicating that a visitor to the REST APIs
+	// has been rate limited.
+	MsgRESTAPIsVisitorGotRateLimited = "[REST APIs] visitor got rate limited"
+)
+
+const (
+
+	// maxRequestRESTAPIsRateLimiter is the maximum number of requests a client can make to
+	// REST API endpoints within the time window specified by maxExpirationRESTAPIsRateLimiter.
+	maxRequestRESTAPIsRateLimiter = 10
+
+	// maxExpirationRESTAPIsRateLimiter is the time window for counting REST API requests
+	// towards the rate limit. After this duration, the request count for each client is reset.
+	maxExpirationRESTAPIsRateLimiter = 1 * time.Minute
 )
---
+++ backend/internal/middleware/restapis_routes.go
@@ -0,0 +1,109 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package middleware
+
+import (
+	"h0llyw00dz-template/backend/internal/database"
+	"h0llyw00dz-template/backend/pkg/restapis/server/health"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// APIRoute represents a single API route, containing the path, HTTP method,
+// handler function, and an optional rate limiter.
+type APIRoute struct {
+	Path        string
+	Method      string
+	Handler     fiber.Handler
+	RateLimiter fiber.Handler
+}
+
+// APIGroup represents a group of API routes under a common prefix.
+// It also allows for a group-wide rate limiter.
+type APIGroup struct {
+	Prefix      string
+	Routes      []APIRoute
+	RateLimiter fiber.Handler
+}
+
+// registerRESTAPIsRoutes registers the REST API routes for the application.
+// It creates a version group ('/v1'), applies rate limiting middleware,
+// and calls the serverAPIs function to register server-related API routes.
+//
+// The function follows the idiomatic Go practices of using descriptive names,
+// handling errors, and using concise and readable code.
+//
+// Parameters:
+//
+//	api: The Fiber router to register the routes on.
+//	db: The database service to be used by the API handlers.
+func registerRESTAPIsRoutes(api fiber.Router, db database.Service) {
+	v1 := api.Group("/v1", func(c *fiber.Ctx) error { // '/v1/' prefix
+		c.Set("Version", "v1")
+		return c.Next()
+	})
+
+	// Apply the rate limiter middleware directly to the REST API routes
+	rateLimiterRESTAPIs := NewRateLimiter(db, maxRequestRESTAPIsRateLimiter, maxExpirationRESTAPIsRateLimiter, MsgRESTAPIsVisitorGotRateLimited)
+
+	// Register server APIs routes
+	serverAPIs(v1, db, rateLimiterRESTAPIs)
+}
+
+// serverAPIs registers the server-related API routes.
+// It defines the API groups and routes, and registers them using the registerGroup function.
+//
+// The function is organized in a clear and concise manner, making it easy to understand
+// and maintain. It follows the idiomatic Go practices of using descriptive names,
+// handling errors, and using a slice to store related data (API groups).
+//
+// Parameters:
+//
+//	v1: The Fiber router to register the routes on (version 1 in this case).
+//	db: The database service to be used by the API handlers.
+//	rateLimiterRESTAPIs: The rate limiter middleware to be applied to the API routes.
+func serverAPIs(v1 fiber.Router, db database.Service, rateLimiterRESTAPIs fiber.Handler) {
+	// Define the API groups and routes
+	apiGroups := []APIGroup{
+		{ // Note: Example https://localhost:8080/v1/server/health/db
+			Prefix:      "/server/health",
+			RateLimiter: rateLimiterRESTAPIs,
+			Routes: []APIRoute{
+				{
+					Path:    "/db",
+					Method:  fiber.MethodGet,
+					Handler: health.DBHandler(db),
+				},
+			},
+		},
+	}
+
+	// Register the API routes for each version
+	for _, group := range apiGroups {
+		registerGroup(v1, group)
+	}
+}
+
+// registerGroup adds all routes from an APIGroup to a specific Fiber router.
+//
+// Parameters:
+//
+//	router: The Fiber router on which to register the group's routes.
+//	group: The APIGroup containing the routes to be registered.
+func registerGroup(router fiber.Router, group APIGroup) {
+	g := router.Group(group.Prefix)
+
+	if group.RateLimiter != nil {
+		g.Use(group.RateLimiter)
+	}
+
+	for _, route := range group.Routes {
+		if route.RateLimiter != nil {
+			g.Add(route.Method, route.Path, route.RateLimiter, route.Handler)
+		} else {
+			g.Add(route.Method, route.Path, route.Handler)
+		}
+	}
+}
