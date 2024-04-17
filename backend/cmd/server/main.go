b0f359a1dab89b7930012deb2bbb2b7cc6f2e70d
H0llyW00dzZ <priv8@btz.pm>
2024-04-17 19:06:49 +0700

    Update Documentation for getEnvVariables
    - [+] feat(main.go): extend getEnvVariables function to include time format and more detailed comments

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -29,8 +29,30 @@ func main() {
 }
 
 // getEnvVariables retrieves essential configuration settings from environment variables.
-// It provides default values for the application name, port, monitoring path, and timeouts
+// It provides default values for the application name, port, monitoring path, time format, and timeouts
 // to ensure the application has sensible defaults if environment variables are not set.
+//
+// The following environment variables are used:
+//
+//   - APP_NAME: The name of the application (default: "Gopher").
+//
+//   - PORT: The port number on which the server will listen (default: "8080").
+//
+//   - MONITOR_PATH: The path for the server monitoring endpoint (default: "/monitor").
+//
+//   - TIME_FORMAT: The format for logging timestamps (default: "unix").
+//
+//     Available options:
+//
+//   - "unix": Unix timestamp format (e.g., [1713355079]).
+//
+//   - "default": Default timestamp format (e.g., 2024/04/17 15:04:05).
+//
+//   - READ_TIMEOUT: The maximum duration for reading the entire request, including the body (default: "5s").
+//
+//   - WRITE_TIMEOUT: The maximum duration before timing out writes of the response (default: "5s").
+//
+//   - SHUTDOWN_TIMEOUT: The maximum duration to wait for active connections to finish during server shutdown (default: "5s").
 func getEnvVariables() (appName, port, monitorPath, timeFormat string, readTimeout, writeTimeout, shutdownTimeout time.Duration) {
 	// Get the APP_NAME, PORT, and MONITOR_PATH from environment variables or use default values.
 	appName = getEnv("APP_NAME", "Gopher")
