20a36c2175aa70807572b9f2497f71fcbdf199ee
H0llyW00dzZ <priv8@btz.pm>
2024-04-28 05:51:04 +0700

    Implement GenerateAPIKey Method for [pkg] [REST APIs] [Helper] (#147)
    - [+] feat(generate_apikey): Implement GenerateAPIKey method

---
+++ backend/pkg/restapis/helper/generate_apikey.go
@@ -0,0 +1,47 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package helper
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+// GenerateAPIKey generates a secure API key using cryptographic techniques.
+// It accepts optional parameters:
+//
+//	length to specify the length of the random byte slice (default is 50)
+//	prefix to specify a custom prefix for the API key (default is "sk-")
+func GenerateAPIKey(options ...interface{}) string {
+	// Set the default length to 50 and default prefix to "sk-"
+	apiKeyLength := 50
+	prefix := "sk-"
+
+	// Parse the optional parameters
+	for _, option := range options {
+		switch v := option.(type) {
+		case int:
+			apiKeyLength = v
+		case string:
+			prefix = v
+		default:
+			panic("Invalid option type for GenerateAPIKey")
+		}
+	}
+
+	// Generate a random byte slice of the specified length
+	randomBytes := make([]byte, apiKeyLength)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		// Note: this not possible.
+		panic(err)
+	}
+
+	// Encode the random bytes using base64
+	apiKey := base64.RawURLEncoding.EncodeToString(randomBytes)
+
+	// Prepend the prefix to the API key
+	return prefix + apiKey
+}
