3175c888803556ea5dc79d761399f1cfcd5fdc8c
H0llyW00dzZ <priv8@btz.pm>
2024-05-05 01:41:23 +0700

    Implement [Internal] [Cryptography] Hybrid Encrypt Cookie
    - [+] feat(hybrid): implement hybrid encryption for secure cookie storage
    - [+] Add the following:
    - [+] hybrid package for hybrid encryption of cookies
    - [+] EncryptCookie function to encrypt cookies using AES-GCM and ChaCha20-Poly1305
    - [+] DecryptCookie function to decrypt cookies using AES-GCM and ChaCha20-Poly1305
    - [+] Service interface for the hybrid encryption service
    - [+] cryptoService struct implementing the Service interface
    - [+] New function to create a new instance of the hybrid encryption service
    - [+] Unit tests for the hybrid encryption functionality
    
    Note: This should work and integrate with the "Fiber EncryptCookie" functionality.

---
+++ backend/internal/middleware/authentication/crypto/hybrid/decryptcookie.go
@@ -0,0 +1,91 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package hybrid
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"encoding/base64"
+
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+// DecryptCookie decrypts a cookie value using a hybrid decryption scheme.
+func DecryptCookie(encodedCookie, key string) (string, error) {
+	keyDecoded, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", ErrorInvalidKey
+	}
+
+	decodedCookie, err := base64.StdEncoding.DecodeString(encodedCookie)
+	if err != nil {
+		return "", ErrorInvalidCookie
+	}
+
+	decryptFn := func(encryptedCookie []byte) ([]byte, error) {
+		// Extract the nonces and encrypted cookie value
+		if len(encryptedCookie) < 12+chacha20poly1305.NonceSize {
+			return nil, ErrorInvalidCookie
+		}
+		aesNonce := encryptedCookie[:12]
+		chachaNonce := encryptedCookie[12 : 12+chacha20poly1305.NonceSize]
+		ciphertext := encryptedCookie[12+chacha20poly1305.NonceSize:]
+
+		// Decrypt the cookie value using ChaCha20-Poly1305
+		aesCiphertext, err := decryptChaCha20Poly1305(ciphertext, chachaNonce, keyDecoded)
+		if err != nil {
+			return nil, err
+		}
+
+		// Decrypt the AES-GCM ciphertext
+		plaintext, err := decryptAESGCM(aesCiphertext, aesNonce, keyDecoded)
+		if err != nil {
+			return nil, err
+		}
+
+		return plaintext, nil
+	}
+
+	plaintext, err := decryptFn(decodedCookie)
+	if err != nil {
+		return "", err
+	}
+
+	return string(plaintext), nil
+}
+
+// decryptAESGCM decrypts the ciphertext using AES-GCM and returns the plaintext.
+func decryptAESGCM(ciphertext, nonce, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+// decryptChaCha20Poly1305 decrypts the ciphertext using ChaCha20-Poly1305 and returns the plaintext.
+func decryptChaCha20Poly1305(ciphertext, nonce, key []byte) ([]byte, error) {
+	aead, err := chacha20poly1305.New(key)
+	if err != nil {
+		return nil, err
+	}
+
+	plaintext, err := aead.Open(nil, nonce, ciphertext, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
---
+++ backend/internal/middleware/authentication/crypto/hybrid/encryptcookie.go
@@ -0,0 +1,86 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package hybrid
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"io"
+
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+// EncryptCookie encrypts a cookie value using a hybrid encryption scheme.
+func EncryptCookie(value, key string) (string, error) {
+	keyDecoded, err := base64.StdEncoding.DecodeString(key)
+	if err != nil {
+		return "", ErrorInvalidKey
+	}
+
+	encryptFn := func(plaintext []byte) ([]byte, error) {
+		// Encrypt the plaintext using AES-GCM
+		ciphertext, aesNonce, err := encryptAESGCM(plaintext, keyDecoded)
+		if err != nil {
+			return nil, err
+		}
+
+		// Encrypt the AES-GCM ciphertext using ChaCha20-Poly1305
+		encryptedCookie, chachaNonce, err := encryptChaCha20Poly1305(ciphertext, keyDecoded)
+		if err != nil {
+			return nil, err
+		}
+
+		// Combine the nonces and encrypted cookie value
+		noncesAndCiphertext := append(aesNonce, chachaNonce...)
+		noncesAndCiphertext = append(noncesAndCiphertext, encryptedCookie...)
+		return noncesAndCiphertext, nil
+	}
+
+	encryptedCookie, err := encryptFn([]byte(value))
+	if err != nil {
+		return "", err
+	}
+
+	encodedCookie := base64.StdEncoding.EncodeToString(encryptedCookie)
+	return encodedCookie, nil
+}
+
+// encryptAESGCM encrypts the plaintext using AES-GCM and returns the ciphertext and nonce.
+func encryptAESGCM(plaintext, key []byte) ([]byte, []byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, nil, err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nonce := make([]byte, aesgcm.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, nil, err
+	}
+
+	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	return ciphertext, nonce, nil
+}
+
+// encryptChaCha20Poly1305 encrypts the plaintext using ChaCha20-Poly1305 and returns the ciphertext and nonce.
+func encryptChaCha20Poly1305(plaintext, key []byte) ([]byte, []byte, error) {
+	aead, err := chacha20poly1305.New(key)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	nonce := make([]byte, aead.NonceSize())
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, nil, err
+	}
+
+	ciphertext := aead.Seal(nil, nonce, plaintext, nil)
+	return ciphertext, nonce, nil
+}
