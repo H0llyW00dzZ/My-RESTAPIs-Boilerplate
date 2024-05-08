8ea93c0c738ce2bfb665ad3ddb73b780dc81de71
H0llyW00dzZ <priv8@btz.pm>
2024-05-08 08:53:23 +0700

    Feat [Internal] [Middleware] [KeyAuth] Session Handling
    - [+] feat(keyauth): add session handling to SuccessKeyAuthHandler
    - [+] retrieve session from context
    - [+] get api_key and api_key_expired from session for encryption
    - [+] works with session middleware for web client-side authentication using cookies

--- backend/internal/middleware/authentication/keyauth/success.go
+++ backend/internal/middleware/authentication/keyauth/success.go
@@ -8,11 +8,23 @@ import (
 	log "h0llyw00dz-template/backend/internal/logger"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/session"
 )
 
 // SuccessKeyAuthHandler is a custom success handler for the key authentication middleware.
 // It logs a message indicating successful API key authentication.
 func SuccessKeyAuthHandler(c *fiber.Ctx) error {
+	// Get the session from the context
+	sess, ok := c.Locals("session").(*session.Session)
+	if !ok {
+		return fiber.ErrInternalServerError
+	}
+
+	// Note: This is for encryption and works in conjunction with the session middleware logic.
+	// It is specifically useful for web front-end client-side authentication using session cookies (e.g., in browsers).
+	// Additionally, it is secure because it would require an 99999999999 cpu to attack this encryptcookie. 🤪
+	sess.Get("api_key")
+	sess.Get("api_key_expired")
 	log.LogUserActivity(c, "API key authenticated successfully")
 	return c.Next()
 }
