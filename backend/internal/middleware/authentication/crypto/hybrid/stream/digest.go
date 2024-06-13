38f519aaab682aba320438fed18020bc5c327da6
H0llyW00dzZ <priv8@btz.pm>
2024-06-13 23:44:24 +0700

    Docs [Crypto] [Hybrid Scheme] Update Comment
    - [+] refactor(stream): add note about saving HMAC sum when HMAC is enabled
    - [+] refactor(stream): update TODO comment to clarify that other hash functions are not needed at the moment

--- backend/internal/middleware/authentication/crypto/hybrid/stream/digest.go
+++ backend/internal/middleware/authentication/crypto/hybrid/stream/digest.go
@@ -11,12 +11,15 @@ import (
 )
 
 // Digest calculates the HMAC digest of the encrypted data (if HMAC is enabled).
+//
+// Note: When HMAC is enabled, don't forget to save/log the HMAC sum into your pocket or whatever it is for future use.
 func (s *Stream) Digest(input io.Reader) ([]byte, error) {
 	if s.hmac == nil {
 		return nil, nil
 	}
 
-	// TODO: Allow Use Other Hash function
+	// TODO: Allow the use of other hash functions. However,
+	// it is not really needed right now since SHA-256 is sufficient on my CPU.
 	hmacHash := hmac.New(sha256.New, s.hmac.Sum(nil))
 
 	if _, err := io.Copy(hmacHash, input); err != nil {
