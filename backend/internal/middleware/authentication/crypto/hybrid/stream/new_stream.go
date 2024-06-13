b490093e2b9c350e19d4f98df7c9e0dc16244cfe
H0llyW00dzZ <priv8@btz.pm>
2024-06-14 05:47:20 +0700

    Docs [Crypto] [Hybrid Scheme] Update Documentation (#360)
    - [+] feat(stream): add documentation for EnableHMAC method
    - [+] refactor(stream): update EnableHMAC method to follow Encrypt-then-MAC approach

--- backend/internal/middleware/authentication/crypto/hybrid/stream/new_stream.go
+++ backend/internal/middleware/authentication/crypto/hybrid/stream/new_stream.go
@@ -41,6 +41,25 @@ func New(aesKey, chachaKey []byte) (*Stream, error) {
 }
 
 // EnableHMAC enables HMAC authentication for the stream using the provided key.
+// The HMAC is computed using SHA-256 and follows the Encrypt-then-MAC approach
+// as specified in (RFC 7366).
+//
+// When HMAC authentication is enabled, the encryption process is modified as follows:
+//  1. The plaintext is encrypted using AES-CTR with a randomly generated nonce.
+//  2. The AES-CTR encrypted data is then encrypted using XChaCha20-Poly1305 with another randomly generated nonce.
+//  3. The HMAC is computed over the XChaCha20-Poly1305 encrypted data.
+//  4. The resulting HMAC tag is appended to the ciphertext.
+//
+// The decryption process is modified as follows:
+//  1. The HMAC tag is extracted from the end of the ciphertext.
+//  2. The HMAC is computed over the remaining ciphertext (excluding the HMAC tag).
+//  3. The computed HMAC is compared with the extracted HMAC tag. If they don't match, an error is returned.
+//  4. If the HMAC verification succeeds, the ciphertext (excluding the HMAC tag) is decrypted using XChaCha20-Poly1305.
+//  5. The resulting data is then decrypted using AES-CTR.
+//
+// Note: The Digest method can be used to calculate the HMAC digest of the encrypted data for future verification.
+// Since the HMAC is computed over both the AES-CTR and XChaCha20-Poly1305 encrypted data, the Digest method is optional
+// and can be used to store the HMAC sum separately for additional verification purposes.
 func (s *Stream) EnableHMAC(key []byte) {
 	s.hmac = hmac.New(sha256.New, key)
 }
