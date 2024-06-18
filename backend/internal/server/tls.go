0e948d7d87a9471e6bd3db41466de6b0e453ab68
H0llyW00dzZ <priv8@btz.pm>
2024-06-19 03:37:47 +0700

    TODO [Server] [Fiber] Hybrid Scheme over TLS 1.3
    - [+] feat(server): add streamConn struct for encrypted communication over TLS 1.3

---
+++ backend/internal/server/tls.go
@@ -0,0 +1,34 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package server
+
+import (
+	"bytes"
+	"crypto/tls"
+	"h0llyw00dz-template/backend/internal/middleware/authentication/crypto/hybrid/stream"
+)
+
+// streamConn is a wrapper struct that combines a TLS 1.3 connection and a Hybrid Scheme (Stream) for encrypted communication.
+//
+// Note: Currently unused, and marked as TODO, will complete implementing this later
+type streamConn struct {
+	*tls.Conn
+	*stream.Stream
+}
+
+// Read reads encrypted data from the TLS connection, decrypts it using the Stream, and returns the decrypted data.
+func (c *streamConn) Read() ([]byte, error) {
+	var buffer bytes.Buffer
+	err := c.Stream.Decrypt(c.Conn, &buffer)
+	if err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+// Write encrypts the provided data using the Stream and writes it to the TLS connection.
+func (c *streamConn) Write(data []byte) error {
+	return c.Stream.Encrypt(bytes.NewReader(data), c.Conn)
+}
