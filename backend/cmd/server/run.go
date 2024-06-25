9466cd08491a513c6181d2f2d985d136e09600bf
H0llyW00dzZ <priv8@btz.pm>
2024-06-25 09:31:04 +0700

    Docs [TLSConfig] Update Documentation

--- backend/cmd/server/run.go
+++ backend/cmd/server/run.go
@@ -140,7 +140,7 @@ func getEnv(key, defaultValue string) string {
 // It sets the minimum TLS version to TLS 1.3 and defines preferred curve preferences.
 // It also uses the GetClientInfo function from the fiber.TLSHandler to get client information.
 //
-// Note: this will override/replace the Fiber default configuration that use TLS 1.2 and use this configuration.
+// Note: this will override/replace the Fiber default configuration that use TLS 1.2 Which is consider outdated & Unsafe now and use this configuration.
 func TLSConfig() *tls.Config {
 	tlsHandler := &fiber.TLSHandler{}
 	return &tls.Config{
