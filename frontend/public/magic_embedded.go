39366aa61524d76ecf48e59a0e235f10029559e6
H0llyW00dzZ <priv8@btz.pm>
2024-07-10 20:55:42 +0700

    Fix Docs

--- frontend/public/magic_embedded.go
+++ frontend/public/magic_embedded.go
@@ -6,7 +6,7 @@ package frontend
 
 import "embed"
 
-// Files is an embedded file system containing the static files from the "images,js" directory.
+// Files is an embedded file system containing the static files from the "images, js" directory.
 //
 // Note: this is a "magic embedded" line and should not be removed, as it is initialized before other code (even another FS System).
 //
