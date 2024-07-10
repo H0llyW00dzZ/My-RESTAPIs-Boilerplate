7bd62ac149af763d63e96245911887dd803024e4
H0llyW00dzZ <priv8@btz.pm>
2024-07-10 15:06:18 +0700

    Implement constant environment variable
    Reason: Easy maintanable.

--- backend/cmd/server/run.go
+++ backend/cmd/server/run.go
@@ -16,6 +16,7 @@ import (
 
 	log "h0llyw00dz-template/backend/internal/logger"
 	handler "h0llyw00dz-template/backend/internal/server"
+	"h0llyw00dz-template/env"
 )
 
 // main is the entry point for the application. It initializes the application
@@ -33,64 +34,20 @@ func main() {
 // getEnvVariables retrieves essential configuration settings from environment variables.
 // It provides default values for the application name, port, monitoring path, time format, and timeouts
 // to ensure the application has sensible defaults if environment variables are not set.
-//
-// Application Configuration:
-//
-//   - APP_NAME: The name of the application (default: "Gopher").
-//   - PORT: The port number on which the server will listen (default: "8080").
-//   - MONITOR_PATH: The path for the server monitoring endpoint (default: "/monitor").
-//   - TIME_FORMAT: The format for logging timestamps (default: "unix").
-//     Available options:
-//   - "unix": Unix timestamp format (e.g., [1713355079]).
-//   - "default": Default timestamp format (e.g., 2024/04/17 15:04:05).
-//
-// Timeout Configuration:
-//
-//   - READ_TIMEOUT: The maximum duration for reading the entire request, including the body (default: "5s").
-//   - WRITE_TIMEOUT: The maximum duration before timing out writes of the response (default: "5s").
-//   - SHUTDOWN_TIMEOUT: The maximum duration to wait for active connections to finish during server shutdown (default: "5s").
-//
-// MySQL Database Configuration:
-//
-//   - DB_HOST: The hostname or IP address of the MySQL database server (required).
-//   - DB_PORT: The port number on which the MySQL database server is listening (required).
-//   - DB_DATABASE: The name of the MySQL database to connect to (required).
-//   - DB_USERNAME: The username for authenticating with the MySQL database (required).
-//   - DB_PASSWORD: The password for authenticating with the MySQL database (required).
-//
-// Redis Database Configuration:
-//
-//   - RDB_ADDRESS: The address of the Redis server (required).
-//   - RDB_PORT: The port number on which the Redis server is listening (required).
-//   - RDB_PASSWORD: The password for authenticating with the Redis server (required).
-//   - RDB_DATABASE: The Redis database number to use (required).
-//   - RDB_POOL_TIMEOUT: The maximum amount of time to wait for a connection from the Redis connection pool (required).
-//   - REDIS_MAXCONN_IDLE_TIME: The maximum amount of time a Redis connection can remain idle in the connection pool (required).
-//   - REDIS_MAXCONN_LIFE_TIME: The maximum lifetime of a Redis connection in the connection pool (required).
-//
-// TLS Configuration:
-//
-//   - EXTRA_CERTS_TLS: Base64-encoded root CA certificates for establishing secure connections database.
-//     This environment variable is used to specify additional root CA certificates that should be trusted by the application.
-//
-// Note: Regarding TLS certificates, there are no environment variables defined. It is necessary to write custom code to handle TLS certificates to avoid conflicts.
-// Also note that "EXTRA_CERTS_TLS" is required for the application that should be trusted (e.g, Private CAs (Recommended for internal))
-// within internal networks used for database connection establishment (For example, in a gateway such as load balancing infrastructure)
-// then this REST API boilerplate can be bound to a external load balancer (e.g., Nginx) for external world exposure.
 func getEnvVariables() (appName, port, monitorPath, timeFormat string, readTimeout, writeTimeout, shutdownTimeout time.Duration) {
 	// Get the APP_NAME, PORT, and MONITOR_PATH from environment variables or use default values.
-	appName = getEnv("APP_NAME", "Gopher")
-	port = getEnv("PORT", "8080")
-	monitorPath = getEnv("MONITOR_PATH", "/monitor")
+	appName = getEnv(env.APPNAME, "Gopher")
+	port = getEnv(env.PORT, "8080")
+	monitorPath = getEnv(env.MONITORPATH, "/monitor")
 	// Get the TIME_FORMAT from environment variables or use default value
 	// Note: List Time Format Available: unix,default
-	timeFormat = getEnv("TIME_FORMAT", "unix")
+	timeFormat = getEnv(env.TIMEFORMAT, "unix")
 
 	// Get the READ_TIMEOUT, WRITE_TIMEOUT, and SHUTDOWN_TIMEOUT from environment variables or use default values.
 	// Note: These default timeout values (5 seconds) are set to help prevent potential deadlocks/hangs.
-	readTimeoutStr := getEnv("READ_TIMEOUT", "5s")
-	writeTimeoutStr := getEnv("WRITE_TIMEOUT", "5s")
-	shutdownTimeoutStr := getEnv("SHUTDOWN_TIMEOUT", "5s")
+	readTimeoutStr := getEnv(env.READTIMEOUT, "5s")
+	writeTimeoutStr := getEnv(env.SHUTDOWNTIMEOUT, "5s")
+	shutdownTimeoutStr := getEnv(env.SHUTDOWNTIMEOUT, "5s")
 
 	// Parse the timeout values into time.Duration
 	readTimeout, _ = time.ParseDuration(readTimeoutStr)
---
+++ env/env.go
@@ -0,0 +1,55 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// License: BSD 3-Clause License
+
+package env
+
+// Application Configuration
+const (
+	APPNAME     = "APP_NAME"     // The name of the application (default: "Gopher").
+	PORT        = "PORT"         // The port number on which the server will listen (default: "8080").
+	MONITORPATH = "MONITOR_PATH" // The path for the server monitoring endpoint (default: "/monitor").
+	TIMEFORMAT  = "TIME_FORMAT"  // The format for logging timestamps (default: "unix").
+	//     Available options:
+	//   - "unix": Unix timestamp format (e.g., [1713355079]).
+	//   - "default": Default timestamp format (e.g., 2024/04/17 15:04:05).
+)
+
+// App/Server Timeout Configuration
+const (
+	READTIMEOUT     = "READ_TIMEOUT"     // The maximum duration for reading the entire request, including the body (default: "5s").
+	WRITETIMEOUT    = "WRITE_TIMEOUT"    // The maximum duration before timing out writes of the response (default: "5s").
+	SHUTDOWNTIMEOUT = "SHUTDOWN_TIMEOUT" // The maximum duration to wait for active connections to finish during server shutdown (default: "5s").
+)
+
+// MySQL Database Configuration
+const (
+	DBHOST     = "DB_HOST"     // The hostname or IP address of the MySQL database server (required).
+	DBPORT     = "DB_PORT"     // The port number on which the MySQL database server is listening (required).
+	DBDATABASE = "DB_DATABASE" // The name of the MySQL database to connect to (required).
+	DBUSERNAME = "DB_USERNAME" // The username for authenticating with the MySQL database (required).
+	DBPASSWORD = "DB_PASSWORD" // The password for authenticating with the MySQL database (required).
+)
+
+// Redis Database Configuration
+const (
+	RDBADDRESS         = "RDB_ADDRESS"             // The address of the Redis server (required).
+	RDBPORT            = "RDB_PORT"                // The port number on which the Redis server is listening (required).
+	RDBPASSWORD        = "RDB_PASSWORD"            // The password for authenticating with the Redis server (required).
+	RDBDATABASE        = "RDB_DATABASE"            // The Redis database number to use (required).
+	RDBPOOLTIMEOUT     = "RDB_POOL_TIMEOUT"        // The maximum amount of time to wait for a connection from the Redis connection pool (required).
+	RDBMAXCONNLIFEIDLE = "REDIS_MAXCONN_IDLE_TIME" // The maximum amount of time a Redis connection can remain idle in the connection pool (required).
+	RDBMAXCONNLIFETIME = "REDIS_MAXCONN_LIFE_TIME" // The maximum lifetime of a Redis connection in the connection pool (required).
+)
+
+// TLS Configuration
+const (
+	// This environment variable is used to specify additional root CA / subs CA certificates that should be trusted by the application.
+	EXTRACERTTLS = "EXTRA_CERTS_TLS" // Base64-encoded root CA / subs CA certificates for establishing secure connections database (required).
+)
+
+// Site Configuration (Optional since it boilerplate and must rewrite a "DomainRouter" in RegisterRoutes (see backend/internal/middleware/routes.go))
+const (
+	DOMAIN       = "DOMAIN"
+	APISUBDOMAIN = "API_SUB_DOMAIN"
+)
