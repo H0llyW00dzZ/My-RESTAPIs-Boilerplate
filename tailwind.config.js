05afaa08a0c5262007e59f8db2957b57e46382be
H0llyW00dzZ <priv8@btz.pm>
2024-07-15 18:09:28 +0700

    Improve [Frontend] [HTMX-TEMPL] Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    // Note: This should be correct. and now it fully rendered.
+    'frontend/htmx/error_page_handler/*.{templ,js}',
+    'frontend/htmx/site/*.{templ,js}',
+    'frontend/public/assets/js/*.{templ,js}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
+  darkMode: 'selector',
 }
-
