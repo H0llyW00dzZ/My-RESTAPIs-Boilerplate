e3ca33a4301d465a9fe3df960e808ffe2985a13e
H0llyW00dzZ <priv8@btz.pm>
2024-07-16 19:41:41 +0700

    Improve [Frontend] [HTMX+TEMPL] Static Handler (#628)

--- frontend/htmx/site/script_templ.go
+++ frontend/htmx/site/script_templ.go
@@ -74,7 +74,7 @@ func ScriptJSClientSide(cspRandom string) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"></script>")
+			_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("\"></script> ")
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
