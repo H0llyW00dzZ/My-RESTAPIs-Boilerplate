649bb03be5afeaecac603c7123d5d3e9f236ae73
H0llyW00dzZ <priv8@btz.pm>
2024-07-22 13:30:04 +0700

    Docs [Worker Generic Configuration] Update Documentation

--- worker/config.go
+++ worker/config.go
@@ -6,13 +6,15 @@
 package worker
 
 // NewDoWorkOption defines a functional option for configuring the worker pool.
+//
+// TODO: Improve this, which it should be configurable for another channel as well (e.g, jobs, results, errors) instead of explicit.
 type NewDoWorkOption[T any] func(*Pool[T])
 
 // WithNumWorkers sets the number of workers in the pool.
 //
 // Example Usage:
 //
-//	pool := worker.NewDoWork[uint32](worker.WithNumWorkers(10))
+//	pool := worker.NewDoWork(worker.WithNumWorkers[uint32](10))
 func WithNumWorkers[T any](numWorkers int) NewDoWorkOption[T] {
 	return func(wp *Pool[T]) { wp.numWorkers = numWorkers }
 }
