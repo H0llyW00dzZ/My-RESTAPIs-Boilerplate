aee5b2156b4402e4ef4b1323b85df68021083ad7
H0llyW00dzZ <priv8@btz.pm>
2024-07-26 13:20:36 +0700

    Improve CSP Header Generator (#663)
    * Implement CSP middleware with configurable options
    
    - [+] feat(middleware): add CSP middleware for generating and setting CSP headers
    - [+] Generate randomness using client IP and additional data
    - [+] Set CSP header with script-src directive and nonce value
    - [+] Store generated randomness in request context for later use
    - [+] Add tests for the CSP middleware with default and custom configurations
    
    * Feat [Backend] [Internal] [Middleware Utils] Add NewCSPHeaderGenerator
    
    - [+] feat(middleware): add WithContextKey option function for middleware configurations
    - [+] feat(middleware): add WithNext option function for middleware configurations
    - [+] refactor(middleware): remove unused digest function
    - [+] feat(middleware): add WithRandomnessGenerator option function for CSP header generator
    - [+] refactor(middleware): move CSP header generator middleware to separate package
    - [+] feat(middleware): add NewCSPHeaderGenerator function for creating CSP header generator middleware
    
    * Improve test
    
    - [+] test(csp_test.go): replace interface{} with any in responseBody type assertion

---
+++ backend/internal/middleware/csp/csp_test.go
@@ -0,0 +1,76 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package csp_test
+
+import (
+	"encoding/json"
+	"h0llyw00dz-template/backend/internal/middleware/csp"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func TestCSPMiddleware(t *testing.T) {
+	app := fiber.New()
+
+	// Test case 1: Default configuration
+	app.Use(csp.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"csp_random": c.Locals("csp_random"),
+		})
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set("X-Real-IP", "192.168.0.1")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if resp.Header.Get("Content-Security-Policy") != "script-src 'nonce-37d7a80604871e579850a658c7add2ae7557d0c6abcc9b31ecddc4424207eba3'" {
+		t.Errorf("Unexpected Content-Security-Policy header value: %s", resp.Header.Get("Content-Security-Policy"))
+	}
+	if resp.Header.Get("Content-Security-Policy") == "" {
+		t.Error("Content-Security-Policy header is empty")
+	}
+
+	// Test case 2: Custom configuration
+	app.Use(csp.New(csp.Config{
+		RandomnessGenerator: func(clientIP string) string {
+			return "custom_randomness"
+		},
+		ContextKey: "custom_csp_key",
+	}))
+
+	app.Get("/custom", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"custom_csp_key": c.Locals("custom_csp_key"),
+		})
+	})
+
+	req = httptest.NewRequest("GET", "/custom", nil)
+	req.Header.Set("CF-Connecting-IP", "127.1.1.1")
+	req.Header.Set("CF-Ray", "12345")
+	req.Header.Set("CF-IPCountry", "US")
+	resp, err = app.Test(req)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+	if resp.Header.Get("Content-Security-Policy") != "script-src 'nonce-custom_randomness'" {
+		t.Errorf("Unexpected Content-Security-Policy header value: %s", resp.Header.Get("Content-Security-Policy"))
+	}
+
+	var responseBody map[string]any
+	err = json.NewDecoder(resp.Body).Decode(&responseBody)
+	if err != nil {
+		t.Fatalf("Failed to decode response body: %v", err)
+	}
+	if responseBody["custom_csp_key"] != "custom_randomness" {
+		t.Errorf("Unexpected custom context key value: %v", responseBody["custom_csp_key"])
+	}
+}
---
+++ backend/internal/middleware/csp/new.go
@@ -0,0 +1,62 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package csp
+
+import (
+	"fmt"
+
+	log "h0llyw00dz-template/backend/internal/logger"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// New creates a new instance of the CSP middleware with the provided configuration.
+func New(config ...Config) fiber.Handler {
+	cfg := DefaultConfig()
+	if len(config) > 0 {
+		cfg = config[0]
+	}
+
+	return func(c *fiber.Ctx) error {
+		// Test Skipped For This when Next True
+		if cfg.Next != nil && cfg.Next(c) {
+			return c.Next()
+		}
+
+		clientIP := c.Get(log.CloudflareConnectingIPHeader)
+		if clientIP == "" {
+			clientIP = c.Get("X-Real-IP")
+			if clientIP == "" {
+				clientIP = c.IP()
+			}
+		}
+
+		cloudflareRayID := c.Get(log.CloudflareRayIDHeader)
+		if cloudflareRayID != "" {
+			clientIP += " - Cloudflare detected - Ray ID: " + cloudflareRayID
+		}
+		countryCode := c.Get(log.CloudflareIPCountryHeader)
+		if countryCode != "" {
+			clientIP += ", Country: " + countryCode
+		}
+
+		// Generate the randomness using the configured generator
+		randomness := cfg.RandomnessGenerator(clientIP)
+
+		// Set the randomness in the context using the configured context key
+		c.Locals(cfg.ContextKey, randomness)
+
+		// Set the CSP header
+		//
+		// Important: Since this CSP header uses a direct digest (using SHA256) without base64 encoding plus immutable. which is idiomatic way.
+		// When using base64 encoding, consider storing the base64 encoded in c.Locals first or somewhere (e.g, database). Avoid fetching the value from
+		// the header and then putting it in the render or direct in the render, as the format will be different due to sanitization.
+		c.Set("Content-Security-Policy", fmt.Sprintf("script-src 'nonce-%s'", randomness))
+
+		// Continue to the next middleware/route handler
+		return c.Next()
+	}
+}
