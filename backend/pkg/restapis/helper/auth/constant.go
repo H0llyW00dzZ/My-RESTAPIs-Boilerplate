108148baabc57991451ecad5ab8e57fa6513d895
H0llyW00dzZ <priv8@btz.pm>
2024-07-29 08:44:05 +0700

    Implement [Helper Function] [REST APIs] "AuthenticateAPIKey"
    - [+] feat(auth): add AuthenticateAPIKey function to retrieve authenticated API key from request context
    - [+] feat(auth): add constant for API key local key name

---
+++ backend/pkg/restapis/helper/auth/constant.go
@@ -0,0 +1,8 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package auth
+
+const apikey = "api_key"
