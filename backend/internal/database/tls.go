9445ed054cc4a7dcf9825026d5852bc55675f355
H0llyW00dzZ <priv8@btz.pm>
2024-07-30 18:34:33 +0700

    Update Documentation
    - [+] docs(database): clarify recommendation to use CA chains instead of only root CA for MySQL and Redis TLS connections

--- backend/internal/database/tls.go
+++ backend/internal/database/tls.go
@@ -15,7 +15,7 @@ import (
 //
 // Note: It is now extracted into two functions for CA certificates because other databases (e.g., Redis) might have a different issuer even if the root CA is the same.
 // The reason for extracting it into two functions is that it makes it easier to set up in a load-balancing cloud environment.
-// It is also recommended to use CA chains instead of only the root CA.
+// It is also recommended to use CA chains (e.g, Root CA + Subs CA Without Leaf CA) instead of only the root CA.
 func loadMySQLRootCA() (*x509.CertPool, error) {
 	rootCABase64 := mysqltlsCAs
 	if rootCABase64 == "" {
@@ -39,7 +39,7 @@ func loadMySQLRootCA() (*x509.CertPool, error) {
 //
 // Note: It is now extracted into two functions for CA certificates because other databases (e.g., MySQL) might have a different issuer even if the root CA is the same.
 // The reason for extracting it into two functions is that it makes it easier to set up in a load-balancing cloud environment.
-// It is also recommended to use CA chains instead of only the root CA.
+// It is also recommended to use CA chains (e.g, Root CA + Subs CA Without Leaf CA) instead of only the root CA.
 func loadRedisRootCA() (*x509.CertPool, error) {
 	rootCABase64 := redistlsCAs
 	if rootCABase64 == "" {
