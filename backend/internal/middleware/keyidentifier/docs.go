1a9c886cc9df2684274cf2a8283356f96779924a
H0llyW00dzZ <priv8@btz.pm>
2024-08-04 19:43:20 +0700

    Update Documentation
    - [+] docs(keyidentifier): clarify incompatibility with Fiber middleware expecting direct key generator value

--- backend/internal/middleware/keyidentifier/docs.go
+++ backend/internal/middleware/keyidentifier/docs.go
@@ -6,6 +6,6 @@
 // Package keyidentifier provides a utility for generating unique keys that can be used for database operations such as Redis/Valkey or key-value stores.
 //
 // Compatibility:
-//   - This package is not directly compatible with Fiber middleware that expects a key generator direct value (e.g, Fiber Rate Limiter).
+//   - This package is not directly compatible with Fiber middleware that expects a key generator direct value (e.g, Fiber Rate Limiter where the ip it used as key not the value).
 //     It generates keys independently of the [fiber.Ctx] and does not modify the context.
 package keyidentifier
