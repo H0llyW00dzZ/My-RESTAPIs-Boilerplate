4a73ac94e71b60621b22d28f63c4187b46793b5a
H0llyW00dzZ <priv8@btz.pm>
2024-08-04 09:57:18 +0700

    Enhance Key Auth + Session Middleware Logic
    - [+] refactor(keyauth): improve API key session handling and data structure
    - [+] Change `isAPIKeyValidInSession` to unmarshal API key data from session using sonic JSON library
    - [+] Modify `saveAPIKeyInSession` to store API key data as JSON in the session
    - [+] Introduce `helper.APIKeyData` struct to represent API key data with status, authorization time, and expiration time
    - [+] Use sonic JSON library for marshaling and unmarshaling API key data in the session
    - [+] Add error handling for JSON marshaling and unmarshaling operations
    - [+] Update comments to provide more context and explanations

--- backend/internal/middleware/authentication/keyauth/validator.go
+++ backend/internal/middleware/authentication/keyauth/validator.go
@@ -8,7 +8,10 @@ package keyauth
 import (
 	"h0llyw00dz-template/backend/internal/database"
 	log "h0llyw00dz-template/backend/internal/logger"
+	"h0llyw00dz-template/backend/internal/middleware/authentication/helper"
+	"time"
 
+	"github.com/bytedance/sonic"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/session"
 )
@@ -28,28 +31,81 @@ func ValidatorKeyAuthHandler(c *fiber.Ctx, key string, db database.Service) (boo
 }
 
 // isAPIKeyValidInSession checks if the API key is valid and not expired in the session.
-// It returns one string values (UUID) and two boolean values: isAPIKeyValid and expired.
+// It returns one string value (UUID) and two boolean values: isAPIKeyValid and expired.
 func isAPIKeyValidInSession(sess *session.Session, key string) (string, bool, bool) {
-	sessionAPIKey := sess.Get("x_api_key")
-	if sessionAPIKey != nil && sessionAPIKey.(string) == key {
-		expiredInSession := sess.Get("x_api_key_expired")
-		if expiredInSession != nil && expiredInSession.(bool) {
-			sess.Destroy()
-			log.LogInfof("API key %s found in session but marked as expired", key)
-			return sess.ID(), true, true
+	sessionAPIKeyData := sess.Get(apiKey)
+	if sessionAPIKeyData != nil {
+		var data helper.APIKeyData
+		// Note: Custom JSON encoder/decoder configuration, similar to what Fiber currently supports,
+		// currently unavailable in this enhancement due to its focus on better performance.
+		//
+		// Idiom Go Error Handling type-assertion.
+		if err := sonic.Unmarshal(sessionAPIKeyData.([]byte), &data); err != nil {
+			log.LogErrorf("Failed to unmarshal API key data from session: %v", err)
+			return "", false, false
 		}
-		return sess.ID(), true, false
+
+		if data.APIKey == key {
+			if data.Status == helper.APIKeyExpired.String() {
+				sess.Destroy()
+				log.LogInfof("API key %s found in session but marked as expired", key)
+				return sess.ID(), true, true
+			}
+			return sess.ID(), true, false
+		}
 	}
 	return "", false, false
 }
 
 // saveAPIKeyInSession saves the API key and its expiration status in the session.
-func saveAPIKeyInSession(sess *session.Session, key string, expired bool) {
-	sess.Set(apiKey, key)
-	sess.Set(apiKeyExpired, expired)
+//
+// Note: The session data is now stored as JSON when viewing in Redis, Valkey Insight, or Commander panel.
+// Additionally, it is possible to implement an encryption/decryption mechanism for the JSON values, as Go, unlike other languages, allows for this functionality + 100% secure.
+func saveAPIKeyInSession(sess *session.Session, key string, expired bool, expirationDate time.Time) {
 	if expired {
+		data := helper.APIKeyData{
+			APIKey: key,
+			Status: helper.APIKeyExpired.String(),
+			Authorization: helper.AuthorizationData{
+				// Time Server not client
+				AuthTime: time.Now().UTC(),
+			},
+		}
+
+		// Note: Custom JSON encoder/decoder configuration, similar to what Fiber currently supports,
+		// currently unavailable in this enhancement due to its focus on better performance.
+		jsonData, err := sonic.Marshal(data)
+		if err != nil {
+			log.LogErrorf("Failed to marshal API key data for Session Middleware: %v", err)
+			return
+		}
+
+		sess.Set(apiKeyExpired, jsonData)
 		sess.SetExpiry(defaultExpryContextKey)
 	}
+
+	data := helper.APIKeyData{
+		APIKey: key,
+		Status: helper.APIKeyActive.String(),
+		Authorization: helper.AuthorizationData{
+			// Time Server not client
+			AuthTime: time.Now().UTC(),
+			// Note: This expiration time is retrieved from the relational database (MySQL).
+			// The performance speed might be somewhat slow (taking an average of 1s response time in the frontend) during the first query due to the relational database (always slow).
+			// However, when it hits Redis and is released into cookies with encryption, the speed can be faster (possibly 0ms ~ 1ms response time).
+			ExpiredTime: expirationDate,
+		},
+	}
+
+	// Note: Custom JSON encoder/decoder configuration, similar to what Fiber currently supports,
+	// currently unavailable in this enhancement due to its focus on better performance.
+	jsonData, err := sonic.Marshal(data)
+	if err != nil {
+		log.LogErrorf("Failed to marshal API key data for Session Middleware: %v", err)
+		return
+	}
+
+	sess.Set(apiKey, jsonData)
 
 	// Note: This is safe if it's encrypted by the Encrypted Cookies middleware (src https://docs.gofiber.io/api/middleware/encryptcookie).
 	// If it's not encrypted, then it's not safe. Also note that when using Encrypted Cookies middleware (src https://docs.gofiber.io/api/middleware/encryptcookie),
