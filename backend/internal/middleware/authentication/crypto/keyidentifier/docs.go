11d23893a8909eb435fc429f41b243dab335c185
H0llyW00dzZ <priv8@btz.pm>
2024-08-06 03:57:31 +0700

    Docs [Crypto] [KeyIdentifier] Update Documentation
    - [+] docs(keyidentifier): add compatibility note about key exchange mechanism not being supported

--- backend/internal/middleware/authentication/crypto/keyidentifier/docs.go
+++ backend/internal/middleware/authentication/crypto/keyidentifier/docs.go
@@ -6,6 +6,9 @@
 // Package keyidentifier provides a utility for generating unique keys that can be used for database operations such as Redis/Valkey or key-value stores.
 //
 // Compatibility:
+//
 //   - This package is not directly compatible with Fiber middleware that expects a key generator direct value (e.g, Fiber Rate Limiter where the ip it used as key not the value).
 //     It generates keys independently of the [fiber.Ctx] and does not modify the context.
+//
+//   - The key exchange mechanism is not supported (and won't be implemented even though implementing key exchange is easy) because it is not used for external services such as TLS or other external services.
 package keyidentifier
