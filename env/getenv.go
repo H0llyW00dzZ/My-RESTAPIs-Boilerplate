43a082819616e8208a2f5e9632badba49a329da9
H0llyW00dzZ <priv8@btz.pm>
2024-08-11 04:21:56 +0700

    Reorganize Codebase for environment variable (#758)
    * Reorganize Codebase for environment variable
    
    - [+] refactor(server): move getEnv function to separate env package
    - [+] feat(env): add GetEnv function to read environment variables with default values
    
    * Docs [GetEnv] Update Documentation
    
    - [+] docs(getenv): fix typo in comment, change "Philosophy Go" to "Go Philosophy"

---
+++ env/getenv.go
@@ -0,0 +1,20 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package env
+
+import "os"
+
+// GetEnv reads an environment variable and returns its value.
+// If the environment variable is not set, it returns a specified default value.
+// This function encapsulates the standard library's [os.LookupEnv] to provide defaults,
+// following the common Philosophy Go idiom of "make the zero value useful".
+func GetEnv(key, defaultValue string) string {
+	value, exists := os.LookupEnv(key)
+	if !exists {
+		return defaultValue
+	}
+	return value
+}
