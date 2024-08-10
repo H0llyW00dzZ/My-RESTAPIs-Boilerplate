e8b550c5488bb97951a8c30a1914cf36b97a4c3c
H0llyW00dzZ <priv8@btz.pm>
2024-08-11 04:20:07 +0700

    Docs [GetEnv] Update Documentation
    - [+] docs(getenv): fix typo in comment, change "Philosophy Go" to "Go Philosophy"

--- env/getenv.go
+++ env/getenv.go
@@ -10,7 +10,7 @@ import "os"
 // GetEnv reads an environment variable and returns its value.
 // If the environment variable is not set, it returns a specified default value.
 // This function encapsulates the standard library's [os.LookupEnv] to provide defaults,
-// following the common Go idiom of "make the zero value useful".
+// following the common Philosophy Go idiom of "make the zero value useful".
 func GetEnv(key, defaultValue string) string {
 	value, exists := os.LookupEnv(key)
 	if !exists {
