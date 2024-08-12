5d7fc89081c7ec68c012309b6f9603baa996ecc7
H0llyW00dzZ <priv8@btz.pm>
2024-08-12 20:57:25 +0700

    Docs [Worker] Update Documentation (#770)
    - [+] docs(worker): add important note about implementing worker goroutines
    - [+] docs(worker): warn about potential resource exhaustion and memory leaks

--- worker/docs.go
+++ worker/docs.go
@@ -4,4 +4,7 @@
 // of the License Agreement, which you can find at LICENSE files.
 
 // Package worker implement goroutine worker just like human being, and it pretty usefull for large go application.
+//
+// Important: Be cautious when implementing worker goroutines that in jobs.
+// Improper implementation can lead to resource exhaustion (e.g., consuming too much memory, smiliar memory leak).
 package worker
