269324d37406b9cf60c781831f6c0d89ca989262
H0llyW00dzZ <priv8@btz.pm>
2024-08-15 12:07:50 +0700

    Implement [Backend] [pkg] MIME Constant Package (#781)
    * Implement [Backend] [pkg] MIME Package
    
    - [+] feat(mime): add mime package with constants for MIME types
    - [+] refactor(helper): use mime constants from new mime package in restapis_error.go
    - [+] test(helper): update tests in restapis_error_test.go to use mime constants
    - [+] docs(mime): add package documentation for mime package
    - [+] chore(middleware): add mime.TextEventStream to allowed content types in routes.go
    
    * Docs [backend] [pkg] [mime] Update Documentation
    
    - [+] refactor(mime): rename MIMETextEventStream constant to TextEventStream

---
+++ backend/pkg/mime/docs.go
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+// Package mime provides constants for MIME types and related implementations and enhancements that are not covered by the standard library or the Fiber package.
+package mime
