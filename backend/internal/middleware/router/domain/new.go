507612b222665120bf7dad83cd7ee9535ef279c8
H0llyW00dzZ <priv8@btz.pm>
2024-08-23 03:22:58 +0700

    Fix Header License
    - [+] docs(domain): update license agreement location in comment

--- backend/internal/middleware/router/domain/new.go
+++ backend/internal/middleware/router/domain/new.go
@@ -1,7 +1,7 @@
 // Copyright (c) 2024 H0llyW00dz All rights reserved.
 //
 // By accessing or using this software, you agree to be bound by the terms
-// of the License Agreement, which you can find in the LICENSE files.
+// of the License Agreement, which you can find at LICENSE files.
 
 package domain
 
