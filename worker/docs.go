f0c9dc4b6519d9f18f566f66a9d98bf72798ab79
H0llyW00dzZ <priv8@btz.pm>
2024-08-26 10:21:55 +0700

    Docs [Worker] Update Documentation (#847)
    - [+] docs(worker): add Kubernetes usage recommendation and average usage stats to docs.go

--- worker/docs.go
+++ worker/docs.go
@@ -7,4 +7,18 @@
 //
 // Important: Be cautious when implementing worker goroutines that in jobs.
 // Improper implementation can lead to resource exhaustion (e.g., consuming too much memory, smiliar memory leak).
+//
+// Recommended: Use this worker in Kubernetes, which is suitable for Horizontal Pod Autoscaling (HPA).
+//
+// Average usage:
+//
+//	NAME                     CPU(cores)   MEMORY(bytes)
+//	senior-golang-worker-775b64c9b5-52t2v   176m         51Mi
+//	senior-golang-worker-775b64c9b5-5rgph   133m         56Mi
+//	senior-golang-worker-775b64c9b5-9d8z5   177m         46Mi
+//	senior-golang-worker-775b64c9b5-bvkpg   146m         42Mi
+//	senior-golang-worker-775b64c9b5-lxrc7   164m         50Mi
+//	senior-golang-worker-775b64c9b5-mkc5k   170m         54Mi
+//	senior-golang-worker-775b64c9b5-pk9gz   175m         47Mi
+//	senior-golang-worker-775b64c9b5-zrcsh   129m         48Mi
 package worker
