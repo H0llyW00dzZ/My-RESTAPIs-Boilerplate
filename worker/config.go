9bd51bb87c48acf4144c01e710d351a198888886
H0llyW00dzZ <priv8@btz.pm>
2024-09-01 07:44:25 +0700

    Enhance [Worker] Default Configuration & Update Documentation
    - [+] refactor(worker): move constants and errors to config.go
    - [+] feat(config.go): add documentation for worker configuration options
    - [+] Document the purpose and usage of worker configuration options
    - [+] Provide recommendations for tuning worker pool settings based on application requirements and performance characteristics
    
    - [+] chore(config.go): update default worker configuration values
    - [+] Adjust NumWorkers constant to 1 (previously 5)
    - [+] Change DefaultWorkerSleepTime constant to 1 minute (previously 1 second)

--- worker/config.go
+++ worker/config.go
@@ -5,7 +5,38 @@
 
 package worker
 
-import "time"
+import (
+	"errors"
+	"time"
+)
+
+var (
+	// ErrFailedToGetSomething is returned when failed to get something..
+	ErrFailedToGetSomething = errors.New("worker failed to get something from job")
+	// ErrJobsNotFound is returned when a job with the specified name is not registered with the worker pool.
+	ErrJobsNotFound = errors.New("job not found")
+)
+
+const (
+	// NumWorkers it for set how many worker, for example I am using 100 worker
+	// that used for handle high traffic + large go application (600+ files) not waste memory.
+	//
+	// Also note that there is the price:
+	//
+	// 100 worker = 100mb ~ 150mb++ memory consumed (Approx)
+	//
+	// under 50 worker still consider cheap.
+	NumWorkers = 1
+)
+
+// Default Worker Configuration
+const (
+	// Note: This Recommended and Suitable for handling long traffic
+	// (e.g, long request http till next billion years then it stop),
+	// high traffic (e.g, many request incoming from different client source, unlike long traffic),
+	// a perfect scheduler (e.g, 24/7 automated updated value for example in database), other worker (e.g, background, assistant garbage collector).
+	DefaultWorkerSleepTime = 1 * time.Minute
+)
 
 // NewDoWorkOption defines a functional option for configuring the worker pool.
 type NewDoWorkOption[T any] func(*Pool[T])
@@ -15,6 +46,10 @@ type NewDoWorkOption[T any] func(*Pool[T])
 // Example Usage:
 //
 //	pool := worker.NewDoWork(worker.WithNumWorkers[uint32](10))
+//
+// Note: It's important to note that increasing the number of workers and buffer sizes can potentially improve the performance and concurrency of the worker pool,
+// but it also depends on the specific use case and the available system resources.
+// It's recommended to tune these values based on go application's requirements and performance characteristics.
 func WithNumWorkers[T any](numWorkers int) NewDoWorkOption[T] {
 	return func(wp *Pool[T]) {
 		wp.numWorkers = numWorkers
@@ -25,6 +60,10 @@ func WithNumWorkers[T any](numWorkers int) NewDoWorkOption[T] {
 type ChanOption[C any] func(ch chan C)
 
 // WithJobChannelOptions configures the job channel.
+//
+// Note: It's important to note that increasing the number of workers and buffer sizes can potentially improve the performance and concurrency of the worker pool,
+// but it also depends on the specific use case and the available system resources.
+// It's recommended to tune these values based on go application's requirements and performance characteristics.
 func WithJobChannelOptions[T any](opts ...ChanOption[Job[T]]) NewDoWorkOption[T] {
 	return func(wp *Pool[T]) {
 		wp.jobChannelOpts = opts
@@ -32,6 +71,10 @@ func WithJobChannelOptions[T any](opts ...ChanOption[Job[T]]) NewDoWorkOption[T]
 }
 
 // WithResultChannelOptions configures the results channel.
+//
+// Note: It's important to note that increasing the number of workers and buffer sizes can potentially improve the performance and concurrency of the worker pool,
+// but it also depends on the specific use case and the available system resources.
+// It's recommended to tune these values based on go application's requirements and performance characteristics.
 func WithResultChannelOptions[T any](opts ...ChanOption[T]) NewDoWorkOption[T] {
 	return func(wp *Pool[T]) {
 		wp.resultChannelOpts = opts
@@ -39,6 +82,10 @@ func WithResultChannelOptions[T any](opts ...ChanOption[T]) NewDoWorkOption[T] {
 }
 
 // WithErrorChannelOptions configures the errors channel.
+//
+// Note: It's important to note that increasing the number of workers and buffer sizes can potentially improve the performance and concurrency of the worker pool,
+// but it also depends on the specific use case and the available system resources.
+// It's recommended to tune these values based on go application's requirements and performance characteristics.
 func WithErrorChannelOptions[T any](opts ...ChanOption[error]) NewDoWorkOption[T] {
 	return func(wp *Pool[T]) {
 		wp.errorChannelOpts = opts
@@ -46,6 +93,9 @@ func WithErrorChannelOptions[T any](opts ...ChanOption[error]) NewDoWorkOption[T
 }
 
 // WithChanBuffer sets the buffer size of a channel.
+//
+// Note: It's important to note that increasing the number of workers and buffer sizes can potentially improve the performance and concurrency of the worker pool, but it also depends on the specific use case and the available system resources.
+// It's recommended to tune these values based on your application's requirements and performance characteristics.
 func WithChanBuffer[C any](bufferSize int) ChanOption[C] {
 	return func(ch chan C) {
 		ch = make(chan C, bufferSize)
@@ -54,6 +104,9 @@ func WithChanBuffer[C any](bufferSize int) ChanOption[C] {
 
 // WithIdleCheckInterval sets the interval at which the worker pool checks
 // for idleness and potentially shuts down.
+//
+// Note: It's important to note that increasing the number of workers and buffer sizes can potentially improve the performance and concurrency of the worker pool, but it also depends on the specific use case and the available system resources.
+// It's recommended to tune these values based on your application's requirements and performance characteristics.
 func WithIdleCheckInterval[T any](interval time.Duration) NewDoWorkOption[T] {
 	return func(wp *Pool[T]) {
 		wp.idleCheckInterval = interval
--- worker/jobs.go
+++ worker/jobs.go
@@ -7,38 +7,8 @@ package worker
 
 import (
 	"context"
-	"errors"
-	"time"
 
 	"github.com/gofiber/fiber/v2"
-)
-
-var (
-	// ErrFailedToGetSomething is returned when failed to get something..
-	ErrFailedToGetSomething = errors.New("worker failed to get something from job")
-	// ErrJobsNotFound is returned when a job with the specified name is not registered with the worker pool.
-	ErrJobsNotFound = errors.New("job not found")
-)
-
-const (
-	// NumWorkers it for set how many worker, for example I am using 100 worker
-	// that used for handle high traffic + large go application (600+ files) not waste memory.
-	//
-	// Also note that there is the price:
-	//
-	// 100 worker = 100mb ~ 150mb++ memory consumed (Approx)
-	//
-	// under 50 worker still consider cheap.
-	NumWorkers = 5
-)
-
-// Default Worker Configuration
-const (
-	// Note: This Recommended and Suitable for handling long traffic
-	// (e.g, long request http till next billion years then it stop),
-	// high traffic (e.g, many request incoming from different client source, unlike long traffic),
-	// a perfect scheduler (e.g, 24/7 automated updated value for example in database), other worker (e.g, background, assistant garbage collector).
-	DefaultWorkerSleepTime = 1 * time.Second
 )
 
 // Job represents a unit of work for the worker pool.
