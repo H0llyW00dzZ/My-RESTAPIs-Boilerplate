7540780586062d4d11c9754a6ed6c3e042c5a7e0
H0llyW00dzZ <priv8@btz.pm>
2024-09-20 01:18:39 +0700

    Docs [pkg] [environment variable] Update Documentation (#947)
    - [+] docs(env): add documentation for creating Kubernetes secrets using create_k8s_secret.sh script

--- env/docs.go
+++ env/docs.go
@@ -4,4 +4,16 @@
 // of the License Agreement, which you can find at LICENSE files.
 
 // Package env provides a constant environment variable for application.
+//
+// Note: For Kubernetes to create secrets, use the bash script "create_k8s_secret.sh" (located in the "k8s-deployment" directory).
+// For example, put "DB_DATABASE=databasename", "DB_USERNAME=dbusername", "DB_PASSWORD=yourdbpasswordpogger" in a file named ".env" or whatever any other desired name.
+//
+// Example ".env" file:
+//
+//	DB_DATABASE=databasename
+//	DB_USERNAME=dbusername
+//	DB_PASSWORD=yourdbpasswordpogger
+//
+// Then run the "create_k8s_secret.sh" script to create the Kubernetes secrets.
+// Note: If your Kubernetes cluster already has a built-in Hardware Security Module (HSM), you don't need to use an external secrets mechanism.
 package env
