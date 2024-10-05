e23fbc34786a6ba66a99f4a0ae614680a9aaa6b1
H0llyW00dzZ <priv8@btz.pm>
2024-10-05 14:54:35 +0700

    Update Comment
    - [+] refactor(middleware): add comment about performance implications of using unique package with worker package

--- backend/internal/middleware/init.go
+++ backend/internal/middleware/init.go
@@ -10,6 +10,7 @@ import "unique"
 // Note: This is just a test of a new package introduced in Go 1.23.
 var uniqueHostnames []unique.Handle[string]
 
+// This should be fine as long as it is not integrated with the worker package, because the unique package uses a mutex that can make it slower.
 func init() {
 	uniqueHostnames = make([]unique.Handle[string], 0)
 }
