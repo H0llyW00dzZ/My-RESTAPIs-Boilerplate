9f10d7e28c02db5f49d240e720d047a52e2114b4
H0llyW00dzZ <priv8@btz.pm>
2024-10-05 20:18:52 +0700

    Improve Unique Caller
    - [+] refactor(csp): move uniqueMake function to separate package gc
    - [+] feat(gc): add UniqueMakeFiberCTX and UniqueMake functions for generating unique handles
    - [+] refactor(csp): update getClientIP to use UniqueMakeFiberCTX for generating unique handle
    
    Note: This a better one

---
+++ backend/pkg/gc/unique.go
@@ -0,0 +1,42 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package gc
+
+import (
+	"unique"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// UniqueMakeFiberCTX is a global variable that holds a function to generate a unique handle for any value.
+// It takes a *fiber.Ctx and a value of any type as input and returns a function that, when called, returns the unique handle value.
+//
+// The unique handle is generated using the unique.Make function, which creates a globally unique identity
+// for the provided value.
+//
+// The returned function, when called, retrieves the actual value of the unique handle using the Value method.
+//
+// This allows for efficient and consistent retrieval of unique handles for any value throughout the code.
+var UniqueMakeFiberCTX = func(c *fiber.Ctx) func(value any) any {
+	return func(value any) any {
+		return unique.Make(value).Value()
+	}
+}
+
+// UniqueMake is a global variable that holds a function to generate a unique handle for any value.
+// It takes a value of any type as input and returns a function that, when called, returns the unique handle value.
+//
+// The unique handle is generated using the unique.Make function, which creates a globally unique identity
+// for the provided value.
+//
+// The returned function, when called, retrieves the actual value of the unique handle using the Value method.
+//
+// This allows for efficient and consistent retrieval of unique handles for any value throughout the code.
+var UniqueMake = func(value any) func() any {
+	return func() any {
+		return unique.Make(value).Value()
+	}
+}
