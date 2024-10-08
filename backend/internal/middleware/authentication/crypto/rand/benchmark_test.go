776e1848da3a6530b56a6df76fa0b0e517705c96
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-10-09 01:04:45 +0800

    Test [Crypto] [Rand Fixed Size] Benchmark
    - [+] feat(authentication): add benchmark tests for crypto/rand package

---
+++ backend/internal/middleware/authentication/crypto/rand/benchmark_test.go
@@ -0,0 +1,61 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+// Test Benchmark on My Laptop
+// Result:
+// goos: windows
+// goarch: amd64
+// pkg: h0llyw00dz-template/backend/internal/middleware/authentication/crypto/rand
+// cpu: Intel(R) Core(TM) i9-10980HK CPU @ 2.40GHz
+// BenchmarkFixedSize32Bytes-16    	 			 5556951	       224.1 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkFixedSizeECDSA/P-256-16         	 5560414	       222.3 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkFixedSizeECDSA/P-384-16         	 4748517	       257.8 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkFixedSizeECDSA/P-521-16         	 4839511	       259.7 ns/op	       0 B/op	       0 allocs/op
+
+package rand_test
+
+import (
+	"crypto/elliptic"
+	"h0llyw00dz-template/backend/internal/middleware/authentication/crypto/rand"
+	"testing"
+)
+
+func BenchmarkFixedSize32Bytes(b *testing.B) {
+	reader := rand.FixedSize32Bytes()
+	buf := make([]byte, 32)
+
+	b.ResetTimer() // Reset the timer to exclude setup time
+	for i := 0; i < b.N; i++ {
+		if _, err := reader.Read(buf); err != nil {
+			b.Fatal(err)
+		}
+	}
+}
+
+func BenchmarkFixedSizeECDSA(b *testing.B) {
+	curves := []elliptic.Curve{
+		elliptic.P256(),
+		elliptic.P384(),
+		elliptic.P521(),
+	}
+
+	for _, curve := range curves {
+		b.Run(curve.Params().Name, func(b *testing.B) {
+			reader := rand.FixedSizeECDSA(curve)
+			expectedSize := 32
+			if curve.Params().BitSize > 256 {
+				expectedSize = 48
+			}
+			buf := make([]byte, expectedSize)
+
+			b.ResetTimer() // Reset the timer to exclude setup time
+			for i := 0; i < b.N; i++ {
+				if _, err := reader.Read(buf); err != nil {
+					b.Fatal(err)
+				}
+			}
+		})
+	}
+}
