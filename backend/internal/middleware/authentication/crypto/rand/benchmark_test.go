6e5e9e126121b4e74d197309e6ed37b1461c5e2d
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-10-10 18:36:33 +0800

    Test [Crypto] [Rand Fixed Size] Update Benchmark Result (#1032)
    - [+] docs(benchmark_test.go): add benchmark results for semi-overclocked state

--- backend/internal/middleware/authentication/crypto/rand/benchmark_test.go
+++ backend/internal/middleware/authentication/crypto/rand/benchmark_test.go
@@ -15,6 +15,17 @@
 // BenchmarkFixedSizeECDSA/P-521-16         	 4839511	       259.7 ns/op	       0 B/op	       0 allocs/op
 //
 // Note: These benchmarks were conducted without overclocking. If overclocked, the performance may reach goes crazy up to 1 billion operations.
+//
+// Additionally, the results shown here are from a semi-overclocked state (3 GHz):
+//
+// goos: windows
+// goarch: amd64
+// pkg: h0llyw00dz-template/backend/internal/middleware/authentication/crypto/rand
+// cpu: Intel(R) Core(TM) i9-10980HK CPU @ 2.40GHz
+// BenchmarkFixedSize32Bytes-16    	 6492642	       191.1 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkFixedSizeECDSA/P-256-16         	 6795412	       184.9 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkFixedSizeECDSA/P-384-16         	 5626008	       214.1 ns/op	       0 B/op	       0 allocs/op
+// BenchmarkFixedSizeECDSA/P-521-16         	 5705864	       211.9 ns/op	       0 B/op	       0 allocs/op
 
 package rand_test
 
