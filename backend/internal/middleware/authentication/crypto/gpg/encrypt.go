10830b01527c30d9d80c5185eb8f1f04b4095953
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-10-22 16:29:24 +0700

    Implement [Crypto] [GPG/OpenPGP] [Helper Function] Streaming Encryption (#1076)
    * Implement [Crypto] [GPG/OpenPGP] [Helper Function] Streaming Encryption
    
    - [+] feat(authentication/crypto/gpg): add file and stream encryption functions
    - [+] test(authentication/crypto/gpg): add tests for file and stream encryption functions
    
    * Fix Test Data
    
    - [+] fix(encrypt_test.go): correct typo in test string from 'OpenGPG' to 'OpenPGP'
    
    * Reformat Docs Test
    
    - [+] docs(encrypt_test.go): add comment line in PGP/GPG keys section

---
+++ backend/internal/middleware/authentication/crypto/gpg/encrypt.go
@@ -0,0 +1,91 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package gpg
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/ProtonMail/gopenpgp/v2/crypto"
+)
+
+// EncryptFile encrypts the given file using the provided PGP public key.
+//
+// TODO: Improve this Use Struct
+func EncryptFile(inputFile, outputFile, publicKey string) error {
+	// Read the public key
+	key, err := crypto.NewKeyFromArmored(publicKey)
+	if err != nil {
+		return fmt.Errorf("failed to read public key: %w", err)
+	}
+
+	keyRing, err := crypto.NewKeyRing(key)
+	if err != nil {
+		return fmt.Errorf("failed to create key ring: %w", err)
+	}
+
+	// Read the input file
+	data, err := os.ReadFile(inputFile)
+	if err != nil {
+		return fmt.Errorf("failed to read input file: %w", err)
+	}
+
+	// Encrypt the data
+	message := crypto.NewPlainMessage(data)
+	encryptedMessage, err := keyRing.Encrypt(message, nil)
+	if err != nil {
+		return fmt.Errorf("failed to encrypt data: %w", err)
+	}
+
+	// Write the encrypted data to the output file
+	if err := os.WriteFile(outputFile, encryptedMessage.GetBinary(), 0644); err != nil {
+		return fmt.Errorf("failed to write encrypted file: %w", err)
+	}
+
+	return nil
+}
+
+// EncryptStream encrypts data from an input stream and writes to an output stream using the provided PGP public key.
+//
+// TODO: Improve this Use Struct
+func EncryptStream(input io.Reader, output io.Writer, publicKey string) error {
+	// Read the public key
+	key, err := crypto.NewKeyFromArmored(publicKey)
+	if err != nil {
+		return fmt.Errorf("failed to read public key: %w", err)
+	}
+
+	keyRing, err := crypto.NewKeyRing(key)
+	if err != nil {
+		return fmt.Errorf("failed to create key ring: %w", err)
+	}
+
+	buffer := make([]byte, 4096) // Define a buffer size
+	for {
+		n, err := input.Read(buffer)
+		if err != nil && err != io.EOF {
+			return fmt.Errorf("failed to read input: %w", err)
+		}
+		if n == 0 {
+			break
+		}
+
+		// Encrypt the data chunk
+		message := crypto.NewPlainMessage(buffer[:n])
+		encryptedMessage, err := keyRing.Encrypt(message, nil)
+		if err != nil {
+			return fmt.Errorf("failed to encrypt data: %w", err)
+		}
+
+		// Write the encrypted data to the output
+		if _, err := output.Write(encryptedMessage.GetBinary()); err != nil {
+			return fmt.Errorf("failed to write encrypted data: %w", err)
+		}
+	}
+
+	return nil
+}
