707422e4edb4a5759a1b120cf37cb814b1d2d476
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-10-22 16:21:44 +0700

    Implement [Crypto] [GPG/OpenGPG] [Helper Function] Streaming Encryption
    - [+] feat(authentication/crypto/gpg): add file and stream encryption functions
    - [+] test(authentication/crypto/gpg): add tests for file and stream encryption functions

---
+++ backend/internal/middleware/authentication/crypto/gpg/encrypt_test.go
@@ -0,0 +1,92 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+package gpg_test
+
+import (
+	"bytes"
+	"h0llyw00dz-template/backend/internal/middleware/authentication/crypto/gpg"
+	"os"
+	"testing"
+)
+
+// Sample PGP/GPG keys for testing
+//
+// KEY:
+// - https://keys.openpgp.org/search?q=95F9A1D43F57344AB88BFFFEA0F9424A7002343A
+//
+// REST APIs GPG Proton Lookup (created by H0llyW00dzZ):
+//
+//	curl -X POST https://api.b0zal.io/v1/gpg/proton/lookup \
+//	-H "Content-Type: application/json" \
+//	-d '{"email":"H0llyW00dzZ@pm.me"}'
+const testPublicKey = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mDMEZhww9xYJKwYBBAHaRw8BAQdAA9nmVRaTTKJe7EDCQ8OhshfDim+9kjCpbUU6
+dSsYkfi0JWgwbGx5dzAwZHp6QHBtLm1lIDxoMGxseXcwMGR6ekBwbS5tZT6IjAQQ
+FgoAPgWCZhww9wQLCQcICZCg+UJKcAI0OgMVCAoEFgACAQIZAQKbAwIeARYhBJX5
+odQ/VzRKuIv//qD5QkpwAjQ6AACUggD+Pm+exMl9WgD7ignm/nW4HXYCyaGe7ZBF
+pILgsOh96twA/122jRFkH5bzcbRjIGuL+9+Nr+69cnuBBtAJNfNFelYPuDgEZhww
+9xIKKwYBBAGXVQEFAQEHQI55aMA1TdV6P/DNh+/TMb3bb1jN7bAlha3HRs5BB9dD
+AwEIB4h4BBgWCgAqBYJmHDD3CZCg+UJKcAI0OgKbDBYhBJX5odQ/VzRKuIv//qD5
+QkpwAjQ6AABELAD/YG153FordpFJMJTI8OEzAvZwRxAvszdvPAMzqI+BSlYBAIBj
+zAozXAC69DgM8AOJzEnsiA55ic1D56y64baz31cD
+=m5PK
+-----END PGP PUBLIC KEY BLOCK-----
+`
+
+func TestEncryptFile(t *testing.T) {
+	// Create a temporary file to encrypt
+	inputFile, err := os.CreateTemp("", "test_input_*.txt")
+	if err != nil {
+		t.Fatalf("Failed to create temporary input file: %v", err)
+	}
+	defer os.Remove(inputFile.Name())
+
+	// Write some data to the input file
+	_, err = inputFile.WriteString("Hello GPG/OpenGPG From H0llyW00dzZ.")
+	if err != nil {
+		t.Fatalf("Failed to write to input file: %v", err)
+	}
+	inputFile.Close()
+
+	// Define the output file
+	outputFile := inputFile.Name() + ".gpg"
+	defer os.Remove(outputFile)
+
+	// Call the EncryptFile function
+	err = gpg.EncryptFile(inputFile.Name(), outputFile, testPublicKey)
+	if err != nil {
+		t.Fatalf("EncryptFile failed: %v", err)
+	}
+
+	// Check if the output file exists
+	if _, err := os.Stat(outputFile); os.IsNotExist(err) {
+		t.Fatalf("Output file was not created")
+	}
+}
+
+func TestEncryptStream(t *testing.T) {
+	// Create a buffer to simulate the input file
+	inputData := []byte("Hello GPG/OpenGPG From H0llyW00dzZ.")
+	inputBuffer := bytes.NewReader(inputData)
+
+	// Create a buffer to simulate the output file
+	outputBuffer := &bytes.Buffer{}
+
+	// Call the EncryptStream function
+	err := gpg.EncryptStream(inputBuffer, outputBuffer, testPublicKey)
+	if err != nil {
+		t.Fatalf("EncryptStream failed: %v", err)
+	}
+
+	// Check if the output buffer has data
+	if outputBuffer.Len() == 0 {
+		t.Fatalf("Output buffer is empty")
+	}
+
+	// Optionally, you can add more checks to see if the data is encrypted
+	// This would typically involve decrypting with a private key and verifying the content
+}
--- go.mod
+++ go.mod
@@ -33,6 +33,10 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/ProtonMail/go-crypto v1.1.0-beta.0-proton // indirect
+	github.com/ProtonMail/go-mime v0.0.0-20230322103455-7d82a3887f2f // indirect
+	github.com/ProtonMail/gopenpgp/v2 v2.7.5 // indirect
+	github.com/ProtonMail/gopenpgp/v3 v3.0.0-beta.2-proton // indirect
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
@@ -44,6 +48,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/charmbracelet/x/ansi v0.2.3 // indirect
 	github.com/charmbracelet/x/term v0.2.0 // indirect
+	github.com/cloudflare/circl v1.3.7 // indirect
 	github.com/cloudflare/cloudflare-go v0.102.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
@@ -103,6 +108,7 @@ require (
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/philhofer/fwd v1.1.3-0.20240612014219-fbbf4953d986 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.6.1 // indirect
 	github.com/prometheus/common v0.55.0 // indirect
 	github.com/prometheus/procfs v0.15.1 // indirect
