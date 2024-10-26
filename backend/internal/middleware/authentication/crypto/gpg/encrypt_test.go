6dbedd35958e57ec1f0af419319774583390f7aa
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-10-26 11:09:37 +0700

    Improve Test [Crypto] [GPG/OpenPGP] [Helper Function] Streaming Encryption (#1130)
    - [+] refactor(encrypt_test.go): remove unused testPublicKeyfromGithub constant due to large
    - [+] docs(encrypt_test.go): add note about GPG Proton's implementation
    - [+] docs(encrypt_test.go): add note about output file extension in TestEncryptStreamToFile

--- backend/internal/middleware/authentication/crypto/gpg/encrypt_test.go
+++ backend/internal/middleware/authentication/crypto/gpg/encrypt_test.go
@@ -54,118 +54,6 @@ lQD3Q4dlnY9UeRlO+wvaMYtg/y9UCpdBWG8qrxyMOw==
 -----END PGP PUBLIC KEY BLOCK-----
 `
 
-// Mirror: https://github.com/H0llyW00dzZ.gpg
-//
-// Note: This large cause RSA
-const testPublicKeyfromGithub = `-----BEGIN PGP PUBLIC KEY BLOCK-----
-
-mQINBGUBn9MBEAC6YMcSxD5IPVSQk/6Kfj0GdJkjhiGx484mZEtqGx2YziLy0wur
-C1zCQIHsq0RdZbBvfxZLeFIH0W6r53bbsohrYIglRjFtkJecY8C1BWa5f9n3lruE
-i8IR7cV+/z5ciBS7LGonE/o60cWGZn5BYccFPvsOcH2JfuFku1+/hqasLnNDoQJd
-6eOZcGx9PLZKSXlfm6UduTtdVd3D8y6LxSSHiBItj3fp+2RT5izlfujWsQOEMuqd
-qw/ODISvlvA26h0PoCm2vMfotNlv6iZw1LHVR1qamDcYFi98IaBB/JhsJhbScufF
-opabk4DGKAWdayILJLUoLdDkYLL5HQkzGB9Pc9X+i+Go7Nuyvnq8TsoMhXdjBmKK
-RfnlJdY3G/pmNyEEXh19Wzu8hr5HpmaUHAEdaDuMho8GK6tq4BUiFSvvuLi4wArl
-AeDXB4subP/4JSUfuubdgT3k4OC0M4V2ppt9XpSp/gcWovSmm4Aaf1n2mX1Em+Ue
-lEHAnfrNlbYBjsI3bnyK3KW7uz2sXAAZCBYAP7qw8CXEfxehRWk+zfZKpnh4l/Ic
-D0Bm+HRpUBi+E4HJXS7Hze6j4Cd+XWKfGMeOM2uJp0/gRw2RHKjJhIdkOMG1Z9vj
-j/C8Ar5Jh/N+zW6JpOogU+P8rhG3W27wI8L+B5Uq3c4yWu4ORKuU/NAjAQARAQAB
-tChIMGxseVcwMGR6WiAoaGVsbDAgdzBybGQpIDxwcml2OEBidHoucG0+iQJRBBMB
-CAA7FiEEPhs0yqXVrDMO6vQABcf//AhFyTAFAmUBn9MCGwMFCwkIBwICIgIGFQoJ
-CAsCBBYCAwECHgcCF4AACgkQBcf//AhFyTCVDA/6AvYdsFPurU84hozQG8d4Mbw8
-IXmuyfZ99Awj+imeqTG2g28OXyz61nxR301cSiaoQefgzRV5CyHAMGDTj/a4gyWb
-JlDK4XJFg2ojG+X6BDSqfXq/5U6T6Qbk7zA7y1AVWCYacMXzN9HzyUk8o1hfVZDY
-y9GRVu/5BDn3an+Q6PlrYSB59efokr8qL55hzItcAU6YR3ynFgv/wYqPSzdZEwhV
-6qZMaqc58o4252gAubD3ryMdJXjXesWiHqbv+EdNXSmavB4hEibi3IThySgso1HF
-ZLsXdynTGYqc6HobktDsKI6xnUSEudWR7ZghhShyhEuus7JDzTx5RNMWV8bAUnK4
-dQCIzBOoANZxiarC0QSozcV/Nb7IqpAIJ2qwBKZvVSd4JlR3GjdM2PZT5z0/nRE4
-TphRYATrAxQTOJi3sC+mz1kIDryOFSNrR8//YneApucjQjOnfr23CXFC8MpzqDlF
-mi1B/jRJJESf36w95+LVCzgAEl6EFuGqKpJF5FAMdHX9TNhw8iXq4JV9CrG+bWF2
-crFOGJ/8YPWzdnKIpYttXlhNvWTW7qo93LV9x7VJqaGrhN+D9CXX6nSGoy09HOGF
-LeUAn0xiZdlzjMDMusRy0U7x2jpp4BqtIevXtXpc6ujwhnuNJwv0pP1KqKlghqfB
-5Zl+8zgO8ESpvzBZ/vS5Ag0EZQGf0wEQAN7W8SYku+vXCg5fffA1ELrQfSGBax1L
-WeesA0Kl5KXMEpPeN80ULdOxxQI+YB1JEHMw9d/mPsvhdkM8c2RMmCqyDxSzn5sD
-BZIWppQJ3KjfjWHUakZKTmYUhSdO+lc2KAAjGrDf5cqfdcctOf/pV2QqO177oBzi
-kLgPklPj0fxeJwfhSlW2V6yVS71XQPZfitcLnk2BHmUbqmK73fVriM0KcFWyBR+m
-+macHONONOSXoKUYTLxfQTmErKVfVRKte4oAc7WDhjCC5UCHP9mlHvz8OqJx1fqD
-VRZaCvTGpJ5pq5BzMWVy0k7iIp97CduASpTItnukgOCNDuJwAdMP6wg7dbR6LuTj
-vJzPXDq9n+7NzyrsJICkQ3cL484ocFqgrJaG9KnH3NGAx9Q552pAVdEt8JCn7czC
-CUlN69/7olnimibLfZ3V3x7mtUJlPkVF9CcW6djnEMPnWsSBSY2gEPHxQ2s9/eHk
-LYhKEBzKtKDC5FUyxOFUUVzwdEfn1CTNNumS0ZAwY1otjvj7DtHXGNzKREk0L5gd
-Byw8tnTFnBSB2UMLexnnLRHBqu/XrGVK6pzVqS4bw6JC5n+gMeXUtI/gDN+At6Uw
-fKDwsDiboMVGncYt3wK4pP9fZhbvmz/Au+uK4q9VV/kYZZYemiv8mg2M45mQ85YK
-j6UHtTTk7NU9ABEBAAGJAjYEGAEIACAWIQQ+GzTKpdWsMw7q9AAFx//8CEXJMAUC
-ZQGf0wIbDAAKCRAFx//8CEXJMBHMD/0c5TPgO3UJn6oDgM06y4ncmOXXuavSzSNP
-P404W/akMMRA2MX9rAdTHnaDTCXatgTGEWWKffK0SrVppfrlYcxuoEtE0d2KbaBo
-TQOGKbp6tBUFeU9ZY7e7LMj5iqm+CVa3Weyl/iq8N44cZoo4t/aPFO9uo7RS4Et5
-CrspmtfF4zlVv8gNSZZZ2pzoWy6D/mgeqkbigpzcD6mfLZS7tSpv3r+vH/4Sp6Wi
-IH0F2oiFb4TJlS29jWhU9Veek/3CcPlYfNn3TCq/VI31Tx8e+yOLbiCwUCzJC3Zi
-YaNiQWNavpWqKKw/w4HBr0FGEwNZ7TXubtVj1sLVjWZz65s3WJLpWTrQyb2RP5/p
-WoT70F7xNi4p+vFQGBpT6UeaU7yYlFqekeD1OuijFxzwFTZ4aDCBt62SZJrqnun9
-9SwVss+dp79qK6vLLwNK1jIFzBMpM27jbbu40ZWxXJ7ELHFfmlZzVz4d5uHTd41S
-ZYiFX+ZSgwlPT7cqqbInJIsx4SgTrXAjU0tmHVK+Y6gb8O8CagI/fTv9hxd5xvZN
-Ov/jSxlV78nTyk8YFbpakGLKy6361UuttJfoLauBx1XK8BIAHyonB7Ah18ztO+3t
-Fo6o2p74SWVmmuGdEvZ8j7QisVG4B9OSrE27ArxG4kuaWOPa2Wf8R8jNZU/VlvcZ
-QMGqigYWFpkCDQRmJrw9ARAAqkIzc3CH5sInr2UuhozNRQd15ucBtd20HrGaO0h5
-ECyrwKyN41JRFz9OusvXkRg9JtHA5Eov8qFKRu3wPHNtLRQWkiN0wwN9Ya9b2hWx
-W1Tlmd7PkdphypFAtrRAGDYSvzn5vgrdnjrIeQUA2YG0oYjU3LhCbDpBqRucspHl
-ztCsibgL3DOb5MDcXBEZ544s2BCLwUDzBf9tiuyRU3EudSNKAvzkqmFxvc6ENrl7
-VEbVFucvJNz2rH/9tBEH+VkgutyV7WETauuVKmKcmOE+Qn8xJzUzpjplsLrd1p34
-OyYfg+S4Z0iyePRFHp+TGCOxJbLsCnoIH9wf91EB1YI0YO+9tFXyBnUXzknJ5s6a
-rUhboGgwcz4To5Km9Z97zWidGiPDf+pAeNUE0ndQ/pLHEt73LZk51agCY89Z7eVT
-q+T9k7JIdUbrEVqDhdWMqzZdhPe1koXB2m5fQM4C+cJwQ6Sug7LfnRH8LGfXgDBj
-v+nIBVtplsUP4Ztp6vRf+5n/Oi6PQDp9hD8LPWM9wRS0o75R/Jw9GRKAkLuv0AqZ
-OaEoH3haqDB8i8EaRuqFbLSMUIaYKsl0JAkjGN40J4Yj3nXn/Fo+Rf+mQnQE3vem
-P6DAm2jck/LuvHDCVX2POblc4WNCZx+oc1qcHW3T66puZGXXZxJCucmycUJv5dZz
-I+UAEQEAAbQmSDBsbHlXMDBkelogKFVuaXgpIDxoMGxseXcwMGR6ekBwbS5tZT6J
-AlEEEwEIADsWIQT/otS9Pss+rEsO8F+wUMVKRgIvswUCZia8PQIbAwULCQgHAgIi
-AgYVCgkICwIEFgIDAQIeBwIXgAAKCRCwUMVKRgIvs/UED/wLQzEo8nDVzzqmZ8Ww
-SC9Hb0EmWlbXCIzj+y16/nnGqlSPWaNYUTDCTglMB9oCC6wHUj2epnAsbmINttYj
-ty3W0tQl6x4tYOEItqVA8XeRZs0wMKg7smrE2aCcS9AtNjzh6CMTU+g2bCDXX5Cg
-7BWhIri1Ol7EuWcOOTslZJugFNi1+WlU5/epVcQXJdTWJ7vjKGHkYyOhYtbfyH/A
-f4S62ByEvRt1NC6whzeduhPCNFLsaLQmIt+JV1nPPfRU0KnrYQV6BIzakJa1TCDM
-n+iBPmOXvAc86OstSFv+55ru11rjzwh8Kvgep9q8X61vFxOpNJT07hVm7yDiAc/e
-gH6xe3RKUu+GxW1N+n2bkfdPWGXkWah9WM1vJnUvm28PthBohnTaddjwo1jx9L/h
-KEMKNvoGjZvIIPYnrO8HWL8PKj5GvANUavytl6zXE72j4Vs5/7suinXZCcbo8lbi
-54Cl5nwaFnunOWHKPK2hYgsuJ4MZR+4ocMBMM+/cPTbSBugqKhKMiFTWJah73fPE
-5qyLok+1I6pQAX8VKxkUZ4JpKnVGvxerTW9aYT/T8GTMvOJSUJDsN0lRo+HR+I+i
-cApauydOLlvXNw0cq4yPLbRKg3zMHzRFCWB3vlncAD49td7/9DMPIOMRYnRU+D8s
-OTsKZ6C7kNS+fk/VKYLBZQx9grkCDQRmJrw9ARAAxIeWC/UIkirhbPyyOiQQ2HKT
-50QOTw6aGJbME8bhGBjRTkGJgg2b0+DVZHNcV5abKa1Q0x5lA3zuvBf11T1j4yEN
-MbUzVqwQq2rarait6kf6tbUJtAQfLB5zLSmzxlpNmQrqw8ZjjfUhLaSTvBEVWFL5
-R8kKECvrst026JjZ8HHa1YGFZuRh/7K+vDNIlik7GDX7yVILFq4NfZykf9nlalWi
-m0kZMoqQKku3FrfowHOAJO9/XIp+S9Wfgjmz8v1o0Tgzux2Hs9Uv79xSI944tiJQ
-MvvJnOVHz+dU+wyhw7Y6wh53xnIyDjRF+8Bp6tdUKHn3eM5Y7azKINkQ5JYF+ZKW
-wWCL+G6DDoo+WqSeS8LbODUZcFkPJReuAPj88W6mrfb1uN5EILExg8Zz7pj5XOqp
-dM3Ej5nCqvrfeprVneDZpNdgtc/N1Kz79SgNPXjF/d6irwYnbuBie0ilKEY00wW6
-xLFhgRNh8/CIpRFa6k53lHZXOf1u5NlGK/Wt76zV8hLksT9gU7q+NZYmtIVlTdL3
-k45lO0LLBWv7h0vQvBq75XOZ3PtFa4eBzIFCIx6Kn1qoogRT/EmtOWbo1dhM4w5b
-bvud+QiX/LZPM7FgAERNVsMp/KD/1ZK+X7uj2FHsz8Ues8MNwdLGlouTI8oYQDjQ
-XbN+6BxoX4axAGaf+RsAEQEAAYkCNgQYAQgAIBYhBP+i1L0+yz6sSw7wX7BQxUpG
-Ai+zBQJmJrw9AhsMAAoJELBQxUpGAi+zesQP/Rfnem+v7fJsJzW0HI+zv7Wc0EyD
-QCtoGijnW2L9baGH9hAEKxh7eLXolU1Tpghs0BLOpcbBellOJceUVuZszX6O7cmM
-YQdSD7aKJfZ4R7mQs2CE0wQtCg7ZnaEkRxzRVOL7PZ6/HkBei6giskdHZO22ycr5
-Dh7gWyPWTYkzge/tNgDhA5HeZ7oUmc+DzjvEGfV5G1R0rSppKdAacotp33lZGKGa
-N3z5/xLdc61zflhQFI0np4vXkBQEsKU01Mi5M0P4rZEEOdvgUoq7a3hTt+s9P1jz
-sNJxFEbb+zVDfH+ALIg9z8duJSTB1mUz0rzsNG7yW3UMXfmq8/3pC/z6veQsSIY3
-IMhJVk1DlR5WrJAbDFNWMWhpKoX1dEOy72yuYnbnvVF4VYkIIjFRSoufa0LF72Tl
-EPuzkaSu043eOycmlJHQu2coxy61Cw3ybVcEjhKI0GpeVAXGijqdlv9lNUNxvLn/
-eJqA2BCwJHGVzFagjjTvTiRu5be4IjsnNBQAxY71FMEOa/R3Qt3vI5bDD1BUh5rD
-sRUvSDKK3e4SQLPu4KX3tB4UjXrr41+TqFy/IiZFNRwE8o1YlMAestoeGj7pYgpf
-iMx/rtWq6Z8fdiaA+VneQ3t7WAb3KETqgSYZX2bG8mptaFRm1ZZIIdNn+/Y0BUTb
-BF2uyUMrStkYS3sExjMEZhww9xYJKwYBBAHaRw8BAQdAA9nmVRaTTKJe7EDCQ8Oh
-shfDim+9kjCpbUU6dSsYkfjNJWgwbGx5dzAwZHp6QHBtLm1lIDxoMGxseXcwMGR6
-ekBwbS5tZT7CjAQQFgoAPgWCZhww9wQLCQcICZCg+UJKcAI0OgMVCAoEFgACAQIZ
-AQKbAwIeARYhBJX5odQ/VzRKuIv//qD5QkpwAjQ6AACUggD+Pm+exMl9WgD7ignm
-/nW4HXYCyaGe7ZBFpILgsOh96twA/122jRFkH5bzcbRjIGuL+9+Nr+69cnuBBtAJ
-NfNFelYPzjgEZhww9xIKKwYBBAGXVQEFAQEHQI55aMA1TdV6P/DNh+/TMb3bb1jN
-7bAlha3HRs5BB9dDAwEIB8J4BBgWCgAqBYJmHDD3CZCg+UJKcAI0OgKbDBYhBJX5
-odQ/VzRKuIv//qD5QkpwAjQ6AABELAD/YG153FordpFJMJTI8OEzAvZwRxAvszdv
-PAMzqI+BSlYBAIBjzAozXAC69DgM8AOJzEnsiA55ic1D56y64baz31cD
-=qodo
------END PGP PUBLIC KEY BLOCK-----
-`
-
 // Mirror (My K8s for Sandbox Development very secure): https://git.b0zal.io/H0llyW00dzZ.gpg
 const testPublicKeyfromGitea = `-----BEGIN PGP PUBLIC KEY BLOCK-----
 
@@ -269,8 +157,8 @@ func TestEncryptStream(t *testing.T) {
 	// Sample public key
 	publicKeys := []string{
 		testPublicKey,
-		// This Both key will be invalid due to the implementation of GPG Proton.
-		testPublicKeyfromGithub,
+		// This key will be invalid due to GPG Proton's implementation
+		// (it does not support a single armored block with multiple keys, so each must be processed individually).
 		testPublicKeyfromGitea,
 	}
 
@@ -321,8 +209,8 @@ func TestGetKeyInfos(t *testing.T) {
 	publicKeys := []string{
 		testPublicKey,
 		testPublicECDSACantEncrypt,
-		// This Both key will be invalid due to the implementation of GPG Proton.
-		testPublicKeyfromGithub,
+		// This key will be invalid due to GPG Proton's implementation
+		// (it does not support a single armored block with multiple keys, so each must be processed individually).
 		testPublicKeyfromGitea,
 	}
 
@@ -373,7 +261,11 @@ func TestEncryptStreamToFile(t *testing.T) {
 	inputBuffer := bytes.NewReader(inputData)
 
 	// Define the output file
-	outputFile, err := os.CreateTemp("", "test_output_*.gpg")
+	//
+	// Note: The output file should include the original file extension before ".gpg".
+	// For example, if the file is a text file, use ".txt.gpg" (e.g., filename.txt.gpg).
+	// This way, when a GPG frontend or other OpenPGP mechanism decrypts it, the result will be filename.txt.
+	outputFile, err := os.CreateTemp("", "test_output_*.txt.gpg")
 	if err != nil {
 		t.Fatalf("Failed to create temporary output file: %v", err)
 	}
