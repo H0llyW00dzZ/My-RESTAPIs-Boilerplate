f08ef8a395920f8ab9f26c2215fabccea58af33f
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-10-30 01:55:38 +0700

    Update Docs [Crypto] [GPG/OpenPGP] [Encrypt Streaming] WithArmor (#1189)
    - [+] docs(gpg/config.go): update comments for WithArmor function to provide more context on on-the-fly encryption

--- backend/internal/middleware/authentication/crypto/gpg/config.go
+++ backend/internal/middleware/authentication/crypto/gpg/config.go
@@ -48,9 +48,16 @@ func WithCompress(compress bool) Option { return func(c *Config) { c.compress = 
 
 // WithArmor sets the option to armor the encrypted message.
 //
-// Note: Default is false to minimize memory allocation, which is effective for on-the-fly encryption (when set false).
+// Note: Default is false to minimize memory allocation, which is effective for on-the-fly encryption
+// and real-time data transmission over a network. When armoring is disabled, the data remains in its
+// binary form, which can be more efficient for streaming large amounts of data.
 //
-// Learn more about how on-the-fly encryption works at: https://www.east-tec.com/kb/safebit/protecting-your-confidential-information/what-does-on-the-fly-encryption-mean/
+// On-the-fly encryption allows data to be encrypted as it is read or written, without the need
+// to store the entire data set in memory. This is particularly useful for applications that require
+// real-time data processing or are constrained by memory resources.
+//
+// For more information about on-the-fly encryption, visit:
+// https://www.east-tec.com/kb/safebit/protecting-your-confidential-information/what-does-on-the-fly-encryption-mean/
 func WithArmor(armor bool) Option { return func(c *Config) { c.armor = armor } }
 
 // WithCustomSuffix sets a custom suffix for the output filename.
