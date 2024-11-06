7e518585513b8920c559975cfcf1fb1812023d52
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-11-07 00:44:44 +0700

    Feat [Go] [Build Binary] New Build Tag "immutable"
    - [+] feat(backend): add new immutable server and route configurations in run_immutable.go and routes_immutable.go

---
+++ backend/cmd/server/run_immutable.go
@@ -0,0 +1,53 @@
+// Copyright (c) 2024 H0llyW00dz All rights reserved.
+//
+// By accessing or using this software, you agree to be bound by the terms
+// of the License Agreement, which you can find at LICENSE files.
+
+//go:build immutable
+// +build immutable
+
+package main
+
+import (
+	"time"
+
+	"github.com/bytedance/sonic"
+	"github.com/gofiber/fiber/v2"
+)
+
+// setupFiber initializes a new Fiber application with custom configuration.
+// It sets up the JSON encoder/decoder, case sensitivity, and strict routing,
+// and applies the application name to the server headers.
+func setupFiber(appName string, readTimeout, writeTimeout time.Duration) *fiber.App {
+	// TODO: Implement a server startup message mechanism similar to "Fiber" ASCII art,
+	// with animation (e.g., similar to a streaming/bubble tea spinner) for multiple sites or large codebases.
+	// The current static "Fiber" ASCII art only shows one site when there are multiple, which isn't ideal.
+	// However, animated ASCII art may not be necessary right now, as it only works properly in terminals.
+	return fiber.New(fiber.Config{
+		ServerHeader: appName,
+		AppName:      appName,
+		// Note: Using the sonic JSON encoder/decoder provides better performance and is more memory-efficient
+		// since Fiber is designed for zero allocation memory usage.
+		JSONEncoder:      sonic.Marshal,
+		JSONDecoder:      sonic.Unmarshal,
+		CaseSensitive:    true,
+		StrictRouting:    true,
+		DisableKeepalive: false,
+		ReadTimeout:      readTimeout,
+		WriteTimeout:     writeTimeout,
+		// Note: It's important to set Prefork to false because if it's enabled and running in Kubernetes,
+		// it may get killed by an Out-of-Memory (OOM) error due to a conflict with the Horizontal Pod Autoscaler (HPA).
+		Prefork: false,
+		// Which is suitable for streaming AI Response.
+		StreamRequestBody:       true,
+		EnableIPValidation:      true,
+		EnableTrustedProxyCheck: true,
+		// By default, it is set to 0.0.0.0/0 for local development; however, it can be bound to an ingress controller/proxy.
+		// This can be a private IP range (e.g., 10.0.0.0/8).
+		TrustedProxies: []string{"0.0.0.0/0"},
+		// Trust X-Forwarded-For headers; additionally, this can be customized if using an ingress controller/proxy, especially Ingress Nginx.
+		ProxyHeader: fiber.HeaderXForwardedFor, // Fix where * (wildcard header) doesn't work in some kubernetes ingress eco-system
+		// Using this immutable setting is more efficient + cheap than the standard library's unique (new pkg).
+		Immutable: true,
+	})
+}
