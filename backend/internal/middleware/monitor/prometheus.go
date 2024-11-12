a61ec95713d33559751c27527a8d8a689018adc9
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-11-12 19:05:00 +0700

    Update Comment
    - [+] docs(prometheus.go): update warning comment about storage mechanism in NewPrometheus function

--- backend/internal/middleware/monitor/prometheus.go
+++ backend/internal/middleware/monitor/prometheus.go
@@ -57,9 +57,9 @@ var DefaultPrometheusConfig = PrometheusConfig{
 
 // NewPrometheus creates a new Prometheus middleware with optional custom configuration options.
 //
-// Warning: Do not use this in production, especially on Kubernetes, as this does not store metrics on disk.
-// The implementation of github.com/ansrivas/fiberprometheus/v2 does not include a storage mechanism,
-// meaning metrics will be stored directly in memory.
+// Warning: Do not use this in production, especially on Kubernetes, as it does not store metrics on disk or utilize a fiber storage mechanism (https://docs.gofiber.io/storage/).
+// The implementation of github.com/ansrivas/fiberprometheus/v2 lacks a storage mechanism,
+// which means metrics will be stored directly in memory.
 func NewPrometheus(config ...PrometheusConfig) fiber.Handler {
 	cfg := DefaultPrometheusConfig
 
