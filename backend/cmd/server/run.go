a75c5c1fc6c0e6855bc6222233721a97c1977c9d
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-11-14 01:50:00 +0700

    Docs [Fiber Configuration] Update Comments (#1287)
    - [+] docs: update comments about X-Forwarded-For headers in various files

--- backend/cmd/server/run.go
+++ backend/cmd/server/run.go
@@ -91,7 +91,10 @@ func setupFiber(config Config) *fiber.App {
 		// By default, it is set to 0.0.0.0/0 for local development; however, it can be bound to an ingress controller/proxy.
 		// This can be a private IP range (e.g., 10.0.0.0/8).
 		TrustedProxies: []string{"0.0.0.0/0"},
-		// Trust X-Forwarded-For headers; additionally, this can be customized if using an ingress controller/proxy, especially Ingress Nginx.
+		// Trust X-Forwarded-For headers. This can be customized if using an ingress controller or proxy, especially Ingress NGINX.
+		//
+		// Note: X-Forwarded-* or any * (wildcard header) from a reverse proxy don't work with Kubernetes Ingress NGINX.
+		// It's better to explicitly use X-Forwarded-For or other specific headers without * (wildcard header).
 		ProxyHeader: fiber.HeaderXForwardedFor, // Fix where * (wildcard header) doesn't work in some kubernetes ingress eco-system
 	})
 }
