463e9345f888d47c314f40c98e5e9d98dafaa54c
H0llyW00dzZ <h0llyw00dzz@pm.me>
2024-12-20 14:44:35 +0700

    Improve [Backend] [Pkg] [Helper Function] [Convert] Error Handling
    - [+] feat(to_bytes.go): add overflow error handling in ToBytes function

--- backend/pkg/convert/to_bytes.go
+++ backend/pkg/convert/to_bytes.go
@@ -6,9 +6,15 @@
 package convert
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 )
+
+// ErrorOverflowOccurred is an error variable that represents an overflow condition during the conversion process.
+// It is returned by the ToBytes function when the converted value exceeds the maximum value that can be stored in an int.
+// This error indicates that the conversion failed due to an overflow and the result cannot be represented accurately.
+var ErrorOverflowOccurred = errors.New("convert: overflow occurred during conversion")
 
 // ToBytes converts a string representation of a size with units (KiB, MiB, GiB, TiB) to an integer number of bytes.
 // It supports both floating-point and non-floating-point values.
@@ -41,6 +47,11 @@ func ToBytes(size string) (int, error) {
 	bytes, err := convertToBytes(num, unitPart)
 	if err != nil {
 		return 0, err
+	}
+
+	// Just in case it overflows, however it is not possible to write a test for this
+	if bytes < 0 {
+		return 0, ErrorOverflowOccurred
 	}
 
 	return bytes, nil
