c3bc07139d2209b948474a8d45bf265902ec24b1
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-01-08 04:35:26 +0700

    Improve Object String
    - [+] fix(archive): import strings package and update file name format validation in NewArchiver function

--- backend/pkg/archive/new.go
+++ backend/pkg/archive/new.go
@@ -5,6 +5,8 @@
 
 package archive
 
+import "strings"
+
 // Archiver represents the archiving functionality with customizable options.
 type Archiver struct {
 	*Config
@@ -26,8 +28,8 @@ func NewArchiver(configs ...Config) *Archiver {
 	// The file name format uses two placeholders: %s for the base file name and %s for the timestamp.
 	// For example, if the base file name is "example.log" and the timestamp is "20230608123456",
 	// the resulting file name will be "example.log_20230608123456.tar.gz".
-	if config.FileNameFormat == "" {
-		config.FileNameFormat = "%s_%s" // Default to %s_%s.tar.gz
+	if config.FileNameFormat == "" || strings.Count(config.FileNameFormat, "%s") < 2 {
+		config.FileNameFormat = defaultFileNameFormat // Default to %s_%s.tar.gz
 	}
 
 	return &Archiver{
