d034df3fc3296d24cbb849a0de90e77386af8477
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-01-29 08:30:27 +0700

    TODO: [Backend] [Helper Function] [Crypto/Rand] [GenerateText] Reduce Complexity
    - [+] docs(rand_text.go): add TODO comment for future improvements in GenerateText function

--- backend/internal/middleware/authentication/crypto/rand/rand_text.go
+++ backend/internal/middleware/authentication/crypto/rand/rand_text.go
@@ -46,6 +46,8 @@ var (
 )
 
 // GenerateText generates a random text string of the specified length and case.
+//
+// TODO: Use a map for better organization and scalability when the complexity exceeds 14 cases (currently 10).
 func GenerateText(length int, textCase TextCase) (string, error) {
 	if length <= 0 {
 		return "", fmt.Errorf("crypto/rand: length %d must be greater than 0", length)
