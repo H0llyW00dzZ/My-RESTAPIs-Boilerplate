e2f0fbafc58f09d62f90fd47cbf9c65c751fab9c
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-04-05 06:17:20 +0700

    Update Docs [Backend] [Helper Function] [Crypto/Rand] "Choice"
    - [+] docs(choice.go): update comments for clarity and add link to cryptography article

--- backend/internal/middleware/authentication/crypto/rand/choice.go
+++ backend/internal/middleware/authentication/crypto/rand/choice.go
@@ -13,10 +13,11 @@ import (
 // Choice selects a random element from the given slice using [crypto/rand] for [secure randomness].
 //
 //   - See [generics] for more information on using generics in Go.
-//   - See [secure randomness] for details on cryptographically secure random number generation.
+//   - See [secure randomness] for details on [cryptographically] secure random number generation.
 //
 // [generics]: https://go.dev/doc/tutorial/generics
 // [secure randomness]: https://en.wikipedia.org/wiki/Cryptographically_secure_pseudorandom_number_generator
+// [cryptographically]: https://en.wikipedia.org/wiki/Cryptography
 func Choice[T any](choices []T) (T, error) {
 	nBig, err := rand.Int(rand.Reader, big.NewInt(int64(len(choices))))
 	if err != nil {
