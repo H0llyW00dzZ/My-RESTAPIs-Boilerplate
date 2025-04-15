975bfeee9348369e21125213262deac53bd956f7
H0llyW00dzZ <h0llyw00dzz@pm.me>
2025-04-16 03:45:11 +0700

    Fix License Header
    - [+] fix(chunk): update copyright year and author name in chunk.go and chunk_test.go

--- backend/pkg/chunk/chunk_test.go
+++ backend/pkg/chunk/chunk_test.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 H0llyW00dz All rights reserved.
+// Copyright (c) 2025 H0llyW00dzZ All rights reserved.
 //
 // By accessing or using this software, you agree to be bound by the terms
 // of the License Agreement, which you can find at LICENSE files.
